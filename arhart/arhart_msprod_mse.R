26f9dd41d585a833694caf42da803b67bbdcec82
ahart1 <arhart555@gmail.com>
2017-01-24 21:36:07 -0500

    Changed N to Nabund so it is easier to search in scripts, sourced calc.indicator script which contains two functions (get.refpts and calc.indicator.hcr) that replace the indicator.hcr function
    Explicitly passed output of eco.results and SShrate.calc functions into variables to be added to ALLResults output
    
    Added code that produces a file that containes initial values for parameters that will be produced once for each set of simulations and saved as "InitialConditions" json file

--- arhart/arhart_msprod_mse.R
+++ arhart/arhart_msprod_mse.R
@@ -7,7 +7,10 @@
 ########  Modifications by Amanda Hart
 ########  Updated: Jan 18, 2017
 
+#N changed to Nabund so easier to search in code, possible that N used in equations may not be changed so it breaks
 
+#Everything is within the Run function (after set working directory and sourcing of functions) which is called in the last line of this script and has no arguments passed in
+#This avoids the problem of having calculated variables saved to global memory rather than being propperly passed in to different functions within the code
 
 # Working directory and datfile source location for "Georges.dat", and "BMSYData", "InitsData", and "IndicatorRefVals" must be changed before running code on new device, these commands rely on directory location of files 
 # For single species assessments a temporary working directory must be provided to run the associated functions, this may need to be reset when switching between computers
@@ -39,10 +42,15 @@ lapply(list.files(pattern = "[.]R$", recursive = TRUE), source)
 setwd("/Users/arhart/Research/ebfm_modeltesting/arhart/")
 # This sources the file contining R scripts that will be regularly used when running arhart_msprod_mse.R contained in arhart folder
 source("UtilityFunctions.R")
+source("calc.indicators.R")
 
 # Set working directory so R scripts previously loaded and all data files are in directory
 setwd("/Users/arhart/Research/ebfm_modeltesting")
 
+
+###########################This is the start of a function (for debugging purposes) that actually runs all parts of model#########################################
+Run <- function()
+  {
 # Read in data files, this location must be changed when running on a new device, values from data file assigned to parameters below
 # Read in BMSY and inits data
 BMSYData <- read.csv("/Users/arhart/Research/ebfm_modeltesting/data/Bmsy.csv", header=TRUE)
@@ -52,7 +60,8 @@ IndicatorRefVals <- read.csv("/Users/arhart/Research/ebfm_modeltesting/data/indi
 datfilename <- "/Users/arhart/Research/ebfm_modeltesting/data/Georges.dat.json"
 dat <- fromJSON(datfilename)
 
-
+#Set number of simulations
+Nsim <- 1
 ##############################################################################
 # Define parameters for use in the model
 ##############################################################################
@@ -93,7 +102,7 @@ BMSY <- BMSY[c(4,5,21,22,14,23,24,6,3,7),]
 BMSY[,2] <- KGuild/2
 
 #initial biomass for each species
-N <- Initvals
+Nabund <- Initvals
 
 ############## get historical time series of biomass and catch, 33 year of data####################
 # Define NI for 33 years of data
@@ -113,20 +122,26 @@ theguilds <- c(1,1,2,2,1,3,3,1,1,1)
 ##############################################################################
 # Note that number of simulations may be changed (I commented out 10,000,000 simulations to run only 10 for ease of fixing minor coding problems)
 
-# Set up a storage object to contain results for each simulation
-ALL.results <- NULL
+# Make a list of indicator chosices to be used in the simulations (same order of control rules used for each maxcat value)
+inds.use.list <- NULL
+for(isim in 1:Nsim)
+{
+inds.use.list[[isim]] <- which.refs(specifyN=FALSE,Nval=8,Nchoose=8)
+}
+
 
 # First for loop runs each simulations through values of maxcatch from 50,000 to 200,000 in steps of 25,000 and allows each of these to be saved to a different file name
 for(maxcat in seq(50000,200000, by=25000))
 {
+  # Set up a storage object to contain results for each simulation
+  ALL.results <- NULL
+  
   # Do a bunch of simulations
-  for(isim in 1:1)
-    #for (isim in 1:10000000)
+  for(isim in 1:Nsim)
   {
     #########determine which of the ecosystem indicators to use in the control rule for  this simulation#########
     #the which.refs code chooses 8 of the possible controle rules for use in each simulation and labels them as inds.use in each simulation run
-    inds.use <- which.refs(specifyN=FALSE,Nval=8,Nchoose=8)
-    
+    inds.use <- inds.use.list[[isim]]
     # This defines the number of years that will be projected forward by the operating model
     Nyr=30
     
@@ -145,21 +160,22 @@ for(maxcat in seq(50000,200000, by=25000))
     ###################################################################################################
     
     ############## calculate values for ecological indicators at start of projection#####################
-    indicators <- eco.indicators(Biomass=NI,Catch=CI,BMSY=KGuild,trophic.level=BMSY[,3],is.predator=which(colSums(alpha)>0),is.pelagic=which(theguilds==2))
-    
+    eco.results <- eco.indicators(Biomass=NI,Catch=CI,BMSY=KGuild,trophic.level=BMSY[,3],is.predator=which(colSums(alpha)>0),is.pelagic=which(theguilds==2))
+    indicators <- eco.results$indicators
+    ei.hist <- eco.results$ei.last
     ######################## set initial values for operating model biomass##################################
     # Defines conditions for year one of the operating model, from which a forward projection will be carried out
     
     # This just adds error to abundance estimates for the first year????????
-    N <- as.numeric(NI[nrow(NI),])*exp(rnorm(10,0,0.2)-0.5*0.2*0.2)
+    Nabund <- as.numeric(NI[nrow(NI),])*exp(rnorm(10,0,0.2)-0.5*0.2*0.2)
     # Target exploitation rate(u)= Target FMSY =Growth rate divided in half
     targ.u <- r/2
     
     # Get the indicator-based reference points based on the chosen indicators using indicator.hcr function, save reference points in xx
-    xx <- indicator.hcr(refvals,limvals,use.defaults=FALSE, get.fmults=FALSE,indvals=ei.hist, RefFile=IndicatorRefVals)
+    xx <- get.refpts(refvals,limvals,use.defaults=FALSE, indvals=ei.hist, RefFile=IndicatorRefVals)
     # Calculate the F multipliers based on the status of ecological indicators compared to reference points
     # The indicator.hcr function does this calculations when get.fmults=TRUE
-    fmult <- indicator.hcr(xx$refvals, xx$limvals, use.defaults=FALSE, get.fmults=TRUE, indvals=ei.hist, RefFile=IndicatorRefVals)
+    fmult <- calc.indicator.hcr(xx$refvals, xx$limvals, use.defaults=FALSE, indvals=ei.hist, RefFile=IndicatorRefVals)
     
 
     ###################################################################################################
@@ -168,7 +184,11 @@ for(maxcat in seq(50000,200000, by=25000))
     # This starts projection in year 2 through Nyr=30(defined in initial values for operating model), initial values for year 1 are defined in previous section of script
     for (iyr in 2:Nyr)
     {    
-      hrate <- SShrate.calc(Nsp,BioObs=cbind(1:nrow(NI.obs),NI.obs),CatObs=cbind(1:nrow(CI.obs),CI.obs),workdir=tempdir, inits=InitsData)
+      SShrate.output <- SShrate.calc(Nsp,BioObs=cbind(1:nrow(NI.obs),NI.obs),CatObs=cbind(1:nrow(CI.obs),CI.obs),workdir=tempdir, inits=InitsData, fmult=fmult, inds.use=inds.use, Nabund=Nabund)
+      hrate <- SShrate.output$hrate
+      SSresults <- SShrate.output$SSresults
+      estu <- SShrate.output$estu
+      u.use <- SShrate.output$u.use
       
       ###########################################This is where the multispecies operating model comes into play######################
      
@@ -178,12 +198,12 @@ for(maxcat in seq(50000,200000, by=25000))
       parms=list(r=r,KGuild=KGuild,Ktot=Ktot,Guildmembership=Guildmembership,BetweenGuildComp=BetweenGuildComp,WithinGuildComp=WithinGuildComp,alpha=alpha,hrate=hrate, maxcat=maxcat)
       # dNbydt is the MSProd model equation
       # If using dNbydt_max instead, also provide maxcat parameter value in parms(above)
-      x <- ode(N,seq(iyr-1,(iyr+0.5),0.5),dNbydt_max,parms=parms,method="rk4")
+      x <- ode(Nabund, seq(iyr-1,(iyr+0.5),0.5), dNbydt_max, parms=parms, method="rk4")
       # Output is X
       # Biomass estimate output
-      N <- x[3,2:(Nsp+1)]
+      Nabund <- x[3,2:(Nsp+1)]
       # N values less than or equal to 0 replaced with 0.01 (Abundance can't be less than or equal to 0)
-      N[N<=0] <- 0.01
+      Nabund[Nabund<=0] <- 0.01
       # Catch estimate output
       Cat <- 1.e-07+x[2,(Nsp+2):(2*Nsp+1)]
       # Catch values less than or equal to 0 replaced with 0.01 (Catch can't be less than or equal to 0)
@@ -201,52 +221,31 @@ for(maxcat in seq(50000,200000, by=25000))
       SS.results <- rbind(SSresults,c(x[1,2:(Nsp+1)],x[2,(Nsp+2):ncol(x)]))
       if (iyr==Nyr) SS.results <- rbind(SS.results,c(x[3,2:(Nsp+1)],x[4,(Nsp+2):ncol(x)]))
       # Add abundance estimate to end of abundance time series
-      NI.nu <- rbind(NI.nu,N)
+      NI.nu <- rbind(NI.nu,Nabund)
       # Add catch estimate to end of catch time series
       CI.nu <- rbind(CI.nu,Cat)
       
       # Generate observed data for this timestep and append to observed dataset
-      Nobs <- N*exp(rnorm(10,0,0.2)-0.5*0.2*0.2)
+      Nobs <- Nabund*exp(rnorm(10,0,0.2)-0.5*0.2*0.2)
       Cobs <- Cat*exp(rnorm(10,0,0.2)-0.5*0.2*0.2)
       NI.obs <- rbind(NI.obs,Nobs)
       CI.obs <- rbind(CI.obs,Cobs)
       
       # Calculate ecological indicators based on new data at this time step
-      ei <- get.Indicators(Biomass=NI.obs,Catch=CI.obs,BMSY=KGuild,trophic.level=BMSY[,3],is.predator=which(colSums(alpha)>0),is.pelagic=which(theguilds==2)) 
-      NCV <- length(ei$div.cv.bio)
-      ei$div.cv.bio <- c(rep(NA,length(ei$tot.bio)-NCV),ei$div.cv.bio)
-      ei <- as.data.frame(ei)
-      ei.now <- as.numeric(ei[nrow(ei),])
-      names(ei.now) = colnames(ei)
-      #indicators <- ei
-      #??????????????????????this last line is the only apparent difference between this section of code and the last
-      #??????????????????????it is not clear to me how/when the output of this second calculation of ei is used
-      #??????????????????????this calculates new values for ei but does not rename to indicators
-      
-      #ORIGINAL CODE
-      # Use get.Indicators function, function name(variable=values associated with variable, ...),  ei is the list of indicators
-      # ei <- get.Indicators(Biomass=NI,Catch=CI,BMSY=KGuild,trophic.level=BMSY[,3],is.predator=which(colSums(alpha)>0),is.pelagic=which(theguilds==2)) 
-      #NCV <- length(ei$div.cv.bio)
-      # This combines a list of NA with ei$div.cv.bio and calls this new list ei$div.cv.bio which overwrites the original list so that it is the same lenght as other files
-      # This is necessary since no div.cv.bio indicator value available from 1975 to 1989
-      # ei$div.cv.bio <- c(rep(NA,length(ei$tot.bio)-NCV), ei$div.cv.bio)
-      # This makes ei a data frame
-      # ei <- as.data.frame(ei)
-      # This returns the last row of ei dataframe
-      #ei.hist <- as.numeric(ei[nrow(ei),])
-      # names(ei.hist) = colnames(ei)
-      #indicators <- ei
-      
+      eco.results <- eco.indicators (Biomass=NI.obs, Catch=CI.obs,BMSY=KGuild,trophic.level=BMSY[,3],is.predator=which(colSums(alpha)>0),is.pelagic=which(theguilds==2))
+      ei.now <- eco.results$ei.last
+      indicators <- eco.results$indicators
+      #why bother with indicators if only ei.now is referenced and ei.now=indicators???????????????
       
       # Work out status relative to refernce points given new indicators.
-      fmult <- indicator.hcr(xx$refvals,xx$limvals,use.defaults=FALSE,get.fmults=TRUE,indvals=ei.now, RefFile=IndicatorRefVals)
+      fmult <- calc.indicator.hcr(xx$refvals,xx$limvals,use.defaults=FALSE, indvals=ei.now, RefFile=IndicatorRefVals)
       #print(c(iyr,estu))
       exprate.est <- rbind(exprate.est,estu)  
       exprate.use <- rbind(exprate.use,u.use)  
     }
     # This is where projection 2:Nyr ends
     # Save results for this simulation, [isim] adds the most recent results to the list
-    ALL.results[[isim]] <- list(targ.u=targ.u,inds.use=inds.use,refvals=xx$refvals,limvals=xx$limvals,estu=exprate.est,u.use=exprate.use,SS.results=SS.results,Nobs=NI.obs,Cobs=CI.obs,ei=ei, maxcat=maxcat)
+    ALL.results[[isim]] <- list(targ.u=targ.u,inds.use=inds.use,refvals=xx$refvals,limvals=xx$limvals,estu=exprate.est,u.use=exprate.use,SS.results=SS.results,Nobs=NI.obs,Cobs=CI.obs,ei=indicators, maxcat=maxcat)
   }
   # This is where simulation loop (total number of simulations we want to run) ends
   
@@ -259,5 +258,14 @@ for(maxcat in seq(50000,200000, by=25000))
   # sprintf() replaces the %d with an integer maxcat, this is called a c-style string formating function
   filename <- paste(location, "arhart", sprintf("results%d.json", maxcat), sep="/")
   write(prettify(ALL.OUTPUT), file = filename)
-
 }
+
+# This produces a file containing the name of the initial data file, and values for the starting parameter values used for the above set of simulations
+TempList <- list(datfilename=datfilename, Nsp=Nsp, Guildmembership=Guildmembership, NGuild=NGuild, Initvals=Initvals, KGuild=KGuild, Ktot=Ktot, hrate=dat$hrate, r=dat$r, BetweenGuildComp=BetweenGuildComp, WithinGuildComp=WithinGuildComp, alpha=alpha, spatial.overlap=spatial.overlap, NI=dat$NI, CI=dat$CI, theguilds=theguilds, BMSYData=BMSYData, InitsData=InitsData, IndicatorRefVals=IndicatorRefVals)
+TempListValues <- toJSON(TempList)
+location <- getwd()
+filename <- paste(location, "arhart/InitialConditions", sep="/")
+write(prettify(TempListValues), file=filename)
+}
+
+Run()
