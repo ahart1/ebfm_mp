9ac8ef90cefcff3a5d6427038043a8361c6d2c9c
ahart1 <arhart555@gmail.com>
2017-01-24 21:27:06 -0500

    Added comments to explain code

--- R/get.Indicators.R
+++ R/get.Indicators.R
@@ -1,16 +1,27 @@
-############# get Indicators
+############# get Indicators##########
+#This script calculated indicator values based on historical data
 
 get.Indicators <- function(Biomass,Catch,size=NULL,trophic.level=NULL,BMSY=NULL,lifespan=NULL,is.predator=NULL,is.pelagic=NULL)
-{
+#??????????????should Biomass and Catch also =NULL so that the values being fed in are used -can do this
+  {
+  # Total system biomass summed over all species
   tot.bio <- rowSums(Biomass,na.rm=TRUE)
+  # Total system catch summed over all species
   tot.cat <- rowSums(Catch,na.rm=TRUE)
+  # Exploitation rate
   exprate <- tot.cat/tot.bio
-  #mean.length <- sum(Biomass*size,na.rm=TRUE)/sum(Biomass,na.rm=TRUE)
-  TL.landings <- rep(NA,nrow(Biomass)) 
+  # mean.length <- sum(Biomass*size,na.rm=TRUE)/sum(Biomass,na.rm=TRUE)
+  # Trophic level of landings
+  TL.landings <- rep(NA,nrow(Biomass))
+  # Trophci level of survey
   TL.survey <- rep(NA,nrow(Biomass)) 
+  # Proportion of total biomass that is comprised by predatory species
   prop.predators <- rowSums(Biomass[,is.predator],na.rm=TRUE)/tot.bio
+  # Pelagic demersal ratio
   pd.ratio <- rowSums(Biomass[,is.pelagic],na.rm=TRUE)/rowSums(Biomass[,-(is.pelagic)],na.rm=TRUE)
+  # Proportion of total biomass that is made of pelagic species
   prop.pel <- 1-(1/(pd.ratio+1))
+  # Proportion of species that is overfished (less than half BMSY)
   prop.overfished <- rep(NA,nrow(Biomass))
   for (i in 1:nrow(Biomass)) 
   {
