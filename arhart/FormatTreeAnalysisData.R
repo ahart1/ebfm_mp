400510a9879abc885ad70d38ed6a40238af2fc72
ahart1 <arhart555@gmail.com>
2017-04-04 15:52:07 -0400

    This creates the function FormatTreeAnalysisData

--- arhart/FormatTreeAnalysisData.R
+++ arhart/FormatTreeAnalysisData.R
@@ -4,100 +4,162 @@
 # Calculates 8 performance metrics for each of 7 catch ceilings 
 # Saves in a table format for use in TreeAnalysis
 
-# CeilingValue should match file name
-#FormatTreeAnalysisData <- function(FileName=NULL, Nsim=NULL, CeilingValue){}
 
-setwd("/Users/arhart/Research/ebfm_modeltesting/arhart")
+FormatTreeAnalysisData <- function(FileName=NULL, Nsim=NULL, CeilingValue=NULL, BMSYData=NULL){
+  # FileName should be in ""
+  # CeilingValue should match file name
+  # BMSYData is data for each species BMSY
+  # Nsim is number of simulation runs stored in FileName 
+  
+  ######## Set up storage 
+  Results <- data.frame()
+  # Set up storage for each Performance metric
+  FreqSSCollapse <- NULL
+  TotSystemBio <- NULL
+  TotSystemCat <- NULL
+  Indicators <- NULL
+  CatchDiversity <- NULL
+  BiomassDiversity <- NULL
+  
+  ######## Load Data and packages
+  library(vegan)
+  library(jsonlite)
+  # datfile variable contains the file name, reads from json file
+  dat <- fromJSON(FileName)
+  
+  ######### Calculate and store performance metrics for each model simulation
+  for(i in 1:Nsim){
+    
+    # Biomass and Catch series for simulation i
+    Biomass <- dat["BiomassResult"][[1]][[i]]  # This should give the first item (matrix of biomass),  for the ith simulation
+    Biomass <- do.call(rbind,Biomass) # This takes the list of lists (JSON format) from the ith simulation run and turns it into a matrix
+    Catch <- dat["CatchResult"][[1]][[i]]  # This should give the first item (matrix of catch) for the ith simulation
+    Catch <- do.call(rbind,Catch) # This takes the list of lists (JSON format) from the ith simulation run and turns it into a matrix
+    
+  
+    #### Calculate performance metrics (Response Variables) for simulation i
+    
+    # Calculate frequency of any single species collapse (below 0.5 BMSY) in the last model year
+    FreqSSCollapse[i] <- length(which(Biomass>BMSYData*0.5))
+    Results[i,"FreqSSCollapse"] <- FreqSSCollapse
+    
+    # Read in BMSY data 
+    #SpeciesBMSY <- BMSYData[c(4,5,21,22,14,23,24,6,3,7),]
+    # use this biomass from file or BMSY=KGuild/2  ????prop.not.collapsed[isim] <- length(which(bio[nrow(bio),]/BMSY[,2]>0.5))/Nsp
+    # if Kguild/2 then i need to read in another data file
+    
+    # below BMSY
+    #which(Biomass)>SpeciesBMSY*0.5
+    # number of species below BMSY
+    #length(which(Biomass[30,])>SpeciesBMSY*0.5)
+    ###########
+    
+    ##### Column3 Calculation
+    
+    ##### Column 4 Calculation
+    
+    # Average Total System Biomass # could I use mean(Biomass) instead?
+    TotSystemBio[i] <- sum(Biomass, na.rm=TRUE)/Nsim
+    Results[i,"TotalSystemBiomass"] <- TotSystemBio
+    ############
+    
+    # Average Total System Catch Removal
+    TotSystemCat[i] <- sum(Catch, na.rm=TRUE)/Nsim
+    Results[i,"TotalSystemCatch"] <- TotSystemCat
+    ############
+    
+    ##### Column 7 Calculation
+    
+    # Biomass diversity in last model year
+    BiomassDiversity[i] <- diversity(Biomass[nrow(Biomass),], index="shannon")
+    Results[i,"BiomassDiversity"] <- BiomassDiversity
+    ############
+    
+    # Catch diversity in last model year
+    library(vegan)
+    CatchDiversity[i] <-  diversity(Catch[nrow(Catch),], index="shannon")
+    Results[i,"CatchDiversity"] <- CatchDiversity
+    ###########
+    
+    # Indicators used
+    Indicators[i] <- dat ["inds.use"]  ##### This may be the wrong syntax since there are multiple things in inds.use
+    Results[i,"Indicators"] <- Indicators
+    ###########
+    
+    #### Format Explanatory Variable Data for simulation i
+    
+    # Catch Ceiling
+    Results[i,"CatchCeiling"] <- CeilingValue
+    
+    # Referance Value Data
+    for(isim in 1:length(dat["refvals"][[1]][[1]])){
+      Results[i,paste(RefVal, isim, sep="")] <- dat["refvals"][[1]][[1]][[isim]]
+    }
+    
+    # Limit Value Data
+    for(isim in 1:length(dat["limvals"][[1]][[1]])){
+      Results[i,paste(LimVal, isim, sep="")] <- dat["limvals"][[1]][[1]][[isim]]
+    }
+    
+  }
+  return(Results=Results)
+}
 
-Nsim <- 5
-
-# Set up storage 
-Results <- data.frame()
-# Set up storage for each Performance metric
-FreqSSCollapse <- NULL
-TotSystemBio <- NULL
-TotSystemCat <- NULL
-Indicators <- NULL
-CatchDiversity <- NULL
-BiomassDiversity <- NULL
 
 
-######## For a single file:
-
-library(jsonlite)
-# datfile variable contains the file name, reads from json file
-#dat <- fromJSON(FileName)
-dat <- fromJSON("sampleformat_results50000.json")
-
-# Biomass and Catch for simulation i
-Biomass <- dat["BiomassResult"][[1]][[i]]  # This should give the first item (matrix of biomass),  for the ith simulation
-Biomass <- do.call(rbind,Biomass) # This takes the list of lists (JSON format) from the ith simulation run and turns it into a matrix
-Catch <- dat["CatchResult"][[1]][[i]]  # This should give the first item (matrix of catch) for the ith simulation
-Catch <- do.call(rbind,Catch) # This takes the list of lists (JSON format) from the ith simulation run and turns it into a matrix
-
-Indicator <- dat["ei"][[1]][[1]] # this is a dataframe from the first simulation
-
-##########
-
-# Catch Ceiling
-Results[i,"CatchCeiling"] <- CeilingValue
-############
-
-# Calculate frequency of any single species collapse (below 0.5 BMSY) in the last model year
-FreqSSCollapse[i] <- length(which(Biomass)>SpeciesBMSY*0.5)
-Results[i,"FreqSSCollapse"] <- FreqSSCollapse
-
-# Read in BMSY data 
-BMSYData <- read.csv("/Users/arhart/Research/ebfm_modeltesting/data/Bmsy.csv", header=TRUE)
-SpeciesBMSY <- BMSYData[c(4,5,21,22,14,23,24,6,3,7),]
-# use this biomass from file or BMSY=KGuild/2  ????prop.not.collapsed[isim] <- length(which(bio[nrow(bio),]/BMSY[,2]>0.5))/Nsp
-# if Kguild/2 then i need to read in another data file
-
-# below BMSY
-which(Biomass)>SpeciesBMSY*0.5
-# number of species below BMSY
-length(which(Biomass[30,])>SpeciesBMSY*0.5)
-###########
-
-##### Column3 Calculation
-
-##### Column 4 Calculation
-
-# Average Total System Biomass # could I use mean(Biomass) instead?
-TotSystemBio[i] <- sum(Biomass, na.rm=TRUE)/Nsim
-Results[i,"TotalSystemBiomass"] <- TotSystemBio
-############
-
-# Average Total System Catch Removal
-TotSystemCat[i] <- sum(Catch, na.rm=TRUE)/Nsim
-Results[i,"TotalSystemCatch"] <- TotSystemCat
-############
-
-##### Column 7 Calculation
-
-# Biomass diversity in last model year
-BiomassDiversity[i] <- diversity(Biomass[30,], index="shannon")
-Results[i,"BiomassDiversity"] <- BiomassDiversity
-############
-
-# Catch diversity in last model year
-library(vegan)
-CatchDiversity[i] <-  diversity(Catch[30,], index="shannon")
-Results[i,"CatchDiversity"] <- CatchDiversity
-###########
-
-# Indicators used
-Indicators[i] <- dat ["inds.use"]  ##### This may be the wrong syntax since there are multiple things in inds.use
-Results[i,"Indicators"] <- Indicators
-###########
-
-
+####################### This next section is me using the function defined above to process my results
 
 # Turn into function
-# run for all ceiling files, add column with ceiling value
+# run for all ceiling files
 # Save output matrix
 # put all matricies in a list
 # do.call(rbind,MatrixList)  OR   rbind(Matrix1,Matrix2, Matrix3...)
+
+
+setwd("/Users/arhart/Research/ebfm_modeltesting/arhart")
+
+# BMSY data used in model(consistent across all simulations and values for catch ceilings)
+BMSYDataInit <- read.csv("/Users/arhart/Research/ebfm_modeltesting/data/Bmsy.csv", header=TRUE)
+SpeciesBMSY <- BMSYDataInit[c(4,5,21,22,14,23,24,6,3,7),]
+# SpeciesBMSY should be passed to the function for the BMSYData argument
+
+Result_Ceiling50000 <- FormatTreeAnalysisData(FileName="sampleformat_results50000.json", Nsim=5, CeilingValue=50000, BMSYData=SpeciesBMSY)
+Result_Ceiling75000 <- FormatTreeAnalysisData(FileName="sampleformat_results75000.json", Nsim=5, CeilingValue=75000, BMSYData=SpeciesBMSY)
+Result_Ceiling100000 <- FormatTreeAnalysisData(FileName="sampleformat_results100000.json", Nsim=5, CeilingValue=100000, BMSYData=SpeciesBMSY)
+Result_Ceiling125000 <- FormatTreeAnalysisData(FileName="sampleformat_results125000.json", Nsim=5, CeilingValue=125000, BMSYData=SpeciesBMSY)
+Result_Ceiling150000 <- FormatTreeAnalysisData(FileName="sampleformat_results150000.json", Nsim=5, CeilingValue=150000, BMSYData=SpeciesBMSY)
+Result_Ceiling175000 <- FormatTreeAnalysisData(FileName="sampleformat_results175000.json", Nsim=5, CeilingValue=175000, BMSYData=SpeciesBMSY)
+Result_Ceiling200000 <- FormatTreeAnalysisData(FileName="sampleformat_results200000.json", Nsim=5, CeilingValue=200000, BMSYData=SpeciesBMSY)
+
+AllResults <- c(Result_Ceiling50000, Result_Ceiling75000, Result_Ceiling100000, Result_Ceiling125000, 
+                Result_Ceiling150000, Result_Ceiling175000, Result_Ceiling200000)
+
+FormattedTreeData <- do.call(rbind, AllResults)
+
+
+
+########################################## This should show the problem with the indicator output
+dat <- fromJSON("sampleformat_results125000.json")
+  
+  # Biomass and Catch series for simulation i
+  Biomass <- dat["BiomassResult"][[1]][[1]]  # This should give the first item (matrix of biomass),  for the ith simulation
+  Biomass <- do.call(rbind,Biomass) # This takes the list of lists (JSON format) from the ith simulation run and turns it into a matrix
+  Catch <- dat["CatchResult"][[1]][[1]]  # This should give the first item (matrix of catch) for the ith simulation
+  Catch <- do.call(rbind,Catch) # This takes the list of lists (JSON format) from the ith simulation run and turns it into a matrix
+  #?????????????????????????? next line needed?
+  Indicator <- dat["ei"][[1]][[1]] # this is a dataframe from the first simulation
+RefVals <- dat["refvals"][[1]][[1]] # this picks out Refvals for first simulation
+
+
+
+
+
+
+
+
+
+
+
 
 
 
