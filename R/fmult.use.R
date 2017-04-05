b4c2a0607b570c8fb510067a808c39629ab2d4ad
ahart1 <arhart555@gmail.com>
2017-04-05 12:17:47 -0400

    Changed argument name which.refs to ChosenRefs so argument will not be confused with the which.refs() function stored in which.refs.R

--- R/fmult.use.R
+++ R/fmult.use.R
@@ -4,13 +4,13 @@
 #?????????????????????????????????does this have more than one column? is fumult a vector (1 column 1 output) or a matrix(multiple column, multiple outputs)? what is a reference point?????????????????
 
 
-fmult.use <- function(fmult,which.refs,type="min")
+fmult.use <- function(fmult,ChosenRefs,type="min")
 {
   #fmult is a matrix containing reference points(1 per row) calculated based on indicators chosed by which.refs script
   #submatrix of fmult is stored as temp (the reference points for the only the chosen indicators(in this case 1-8 were chosen) are stored as temp)
-  temp <- fmult[which.refs,]
+  temp <- fmult[ChosenRefs,]
   #if there is more than 1 row, use apply to execute function named "type" on each column( in this example take the minimum of each column)
-  if(length(which.refs)>1) fmult.use <- apply(temp,2,type,na.rm=TRUE)
-  if(length(which.refs)==1) fmult.use <- apply(matrix(temp,nrow=1),2,type,na.rm=TRUE)
+  if(length(ChosenRefs)>1) fmult.use <- apply(temp,2,type,na.rm=TRUE)
+  if(length(ChosenRefs)==1) fmult.use <- apply(matrix(temp,nrow=1),2,type,na.rm=TRUE)
   return(fmult.use)
 }
