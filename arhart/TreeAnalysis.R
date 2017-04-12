e4bb6b330088a7f5438246c8af875e57f07124a1
ahart1 <arhart555@gmail.com>
2017-04-12 14:18:43 -0400

    This is the final (I hope) version of the TreeAnalysis.R script which first produces a tree, runs cross validation to pick optimal number of splits, and then produces a second tree with the optimal number of splits.
    There are three files that are saved as output:
    1) TreeResults which includes written structure for the initial tree with high complexity.
    2) TreeCPResults which includes the diagnostic table used to pick optimal number of splits, printcp() will give the same results along with other tree info but is less useful for use in the code
    3) OptimalTreeResults which produces the same type of results as 1) but for the optimal tree instead
    
    Graphs produced by this script are printed but not saved, this may be done by hand if desired

--- arhart/TreeAnalysis.R
+++ arhart/TreeAnalysis.R
@@ -8,8 +8,12 @@ TreeAnalysis <- function(DataFile=NULL,NPerformMetrics=NULL){
   Data <- read.table(DataFile)
   # Load programs
   library(rpart)
-  # Set up storage for trees
+  # Set up storage for trees and pruning cross validation
   TreeResults <- list()
+  CPResult <- list()
+  OptimalSplits <- list()
+  OptimalTreeResults <- list()
+  
   
   # Define list of Performance Metrics
   PerformMet <- colnames(Data[1:NPerformMetrics])
@@ -18,16 +22,15 @@ TreeAnalysis <- function(DataFile=NULL,NPerformMetrics=NULL){
     tryCatch({
       
     
-      # Produce Initial Tree and plot a variety of cp(different tree complexities)
-      Tree <- rpart(Data[,i] ~ CatchCeiling + # this only works when name explicitly included
+      ####################### Produce Initial Tree ###########################################################################
+      Tree <- rpart(Data[,i] ~ as.factor(CatchCeiling) + # this only works when name explicitly included
                       RefVal1 + RefVal2 + RefVal3 + RefVal4 + 
                       RefVal5 + RefVal6 + RefVal7 + RefVal8 +
                       LimVal1 + LimVal2 + LimVal3 + LimVal4 +
                       LimVal5 + LimVal6 + LimVal7 + LimVal8,
                     data = Data,
                     control = rpart.control(cp=0.001)) # cp=complexity parameter, splits that don't decrease lack of fit by 0.001 not attempted
-      # Print output
-      Tree
+
       # Produce tree graphic
       par(xpd = NA, mar = c(2.5, 5, 2.5, 5)) # sets up graphing space so no labels are cut off
       plot(Tree,  main=paste(PerformMet[i],sep=""))
@@ -37,49 +40,33 @@ TreeAnalysis <- function(DataFile=NULL,NPerformMetrics=NULL){
       # Store Tree
       TreeResults[[i]] <- Tree
       
-      # Plot variety of cp (different tree complexities)
+      ################### Plot variety of cp (different tree complexities) and pick optimal complexity###########################################
       par(mar = c(4.5, 4.5, 2.5, 0.5))
       plotcp(Tree, main=paste(PerformMet[i],sep=""))
-      # Print pruning output
-      printcp(Tree)
+      # Store pruning cross validation output
+      CPResult[[i]] <- Tree$cptable
+      #CPResult[[i]] <- capture.output(printcp(TreeResults[[i]]))
       par(mar = c(4.5, 4.5, 0.5, 0.5))
-
-    },
-    error=function(e){
-      print("fit is not a tree, just a root") # e is a conditional object created by the code to store the error
-    })
-  }
-  return(TreeResults)
-  #write(TreeResults, file="TreeResults")
-}
-
-
-
-# OptimalVals should be a list
-
-OptimalTreeAnalysis <- function(DataFile=NULL,NPerformMetrics=NULL,OptimalVals=NULL){
-  ##### Run regression tree #####
-  # Read in Formatted Data
-  Data <- read.table(DataFile)
-  # Load programs
-  library(rpart)
-  # Set up storage for trees
-  OptimalTreeResults <- list()
-  
-  # Define list of Performance Metrics
-  PerformMet <- colnames(Data[1:NPerformMetrics])
-  
-  for(i in 1:NPerformMetrics){
-    tryCatch({
       
-      # Produce optimal tree (give optimal number of splits)
-      OptimalTree <- rpart(UsePerformMet ~ CatchCeiling + 
+      
+      # Pick optimal complexity (leftmost cp which is within 1 std error of the minimum error (the plotted line))
+      MinError <-min(rowSums(CPResult[[i]][,c("xerror","xstd")])) # minimum error represents the line drawn when plotcp() is run
+      PickCP <- min(which(CPResult[[i]][,"xerror"] < MinError)) # gives position of xerrors that meet are less than MinError, pick the minimum position (the leftmost cp on graph that is below dotted line(MinError))
+      OptimalSplits[[i]] <- CPResult[[i]][PickCP,"nsplit"]
+      
+      if(OptimalSplits[[i]]==0){
+        OptimalSplits[[i]] <- OptimalSplits[[i]] + 1
+      }
+      
+      ############# Update to produce Optimal Tree (optimal complexity)############################
+      # Add minsplit to rpart() to force optimal number of splits
+      OptimalTree <- rpart(Data[,i] ~ as.factor(CatchCeiling) + 
                              RefVal1 + RefVal2 + RefVal3 + RefVal4 + 
                              RefVal5 + RefVal6 + RefVal7 + RefVal8 + 
                              LimVal1 + LimVal2 + LimVal3 + LimVal4 +
                              LimVal5 + LimVal6 + LimVal7 + LimVal8,
                            data = Data,
-                           control = rpart.control(minsplit=OptimalVals[i],cp=0.001)) 
+                           control = rpart.control(minsplit=OptimalSplits[[i]],cp=0.001)) 
       # cp=complexity parameter, splits that don't decrease lack of fit by 0.001 not attempted
       # minslplit set to optimal value determined using TreeAnalysis
       
@@ -98,7 +85,13 @@ OptimalTreeAnalysis <- function(DataFile=NULL,NPerformMetrics=NULL,OptimalVals=N
       print("fit is not a tree, just a root") # e is a conditional object created by the code to store the error
     })
   }
+  #return(TreeResults)
+  capture.output(print(TreeResults), file="TreeResults")
+  capture.output(print(CPResult), file="TreeCPResults")
+  capture.output(print(OptimalTreeResults), file="OptimalTreeResults")
 }
+
+
 
 ####### This uses the TreeAnalysis function above to create trees based on the given Performance metric
 setwd("/Users/ahart2/Research/ebfm_mp/arhart/BioStats_Sim1000_AllInds")
@@ -107,7 +100,5 @@ TreeAnalysis(DataFile="FormattedTreeData_BioStats_Sim1000_AllInds", NPerformMetr
 
 
 
-# Still not certain what par() does
-# How do you figure out what optimal cp is?
-
+  
 
