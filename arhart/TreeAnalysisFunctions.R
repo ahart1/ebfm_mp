5c8182b70fae2c82cf009e9be666eb3b8fa96f5f
ahart1 <arhart555@gmail.com>
2017-05-08 14:53:36 -0400

    This script contains all functions needed to perform a simple regression/classification tree using data output from a multispecies production model (with biomass and catch time series). It is a combination of FormatTreeAnalysisData.R, TreeAnalysis.R, and Boxplots.R (this last file will likely be renamed to TreeBoxplots.R)

---
+++ arhart/TreeAnalysisFunctions.R
@@ -0,0 +1,278 @@
+# This script contains functions to format data and run a regression/classification tree analysis on that data
+
+##################### FormatTreeAnalysisData ##########################################################################
+
+# This script processes the output from arhart_msprod_mse.R 
+# Calculates 8 performance metrics for each of 7 catch ceilings 
+# Saves in a table format for use in TreeAnalysis
+# Nsim and CeilingValue and BMSY arguments should match source file production
+
+
+FormatTreeAnalysisData <- function(FileName=NULL, Nsim=NULL, CeilingValue=NULL, BMSY=NULL){
+  # FileName should be in ""
+  # CeilingValue should match file name
+  # BMSYData is data for each species BMSY
+  # Nsim is number of simulation runs stored in FileName 
+  
+  ######## Set up storage 
+  Results <- data.frame()
+  # Set up storage for each Performance metric
+  FreqSSOverfished <- NULL
+  TotSystemBio <- NULL
+  TotSystemCat <- NULL
+  CatchDiversity <- NULL
+  BiomassDiversity <- NULL
+  
+  ######## Load Data and packages
+  library(vegan)
+  library(jsonlite)
+  # datfile variable contains the file name, reads from json file
+  dat <- fromJSON(FileName)
+  
+  ######### Calculate and store performance metrics for each model simulation
+  for(i in 1:Nsim){
+    
+    # Biomass and Catch series for simulation i
+    Biomass <- dat["BiomassResult"][[1]][[i]]  # This should give the first item (matrix of biomass),  for the ith simulation
+    #Biomass <- do.call(rbind,Biomass) # This takes the list of lists (JSON format) from the ith simulation run and turns it into a matrix
+    Catch <- dat["CatchResult"][[1]][[i]]  # This should give the first item (matrix of catch) for the ith simulation
+    #Catch <- do.call(rbind,Catch) # This takes the list of lists (JSON format) from the ith simulation run and turns it into a matrix
+    
+    
+    #### Calculate performance metrics (Response Variables) for simulation i
+    
+    # Calculate frequency of any single species collapse (below 0.5 BMSY) in the last model year
+    Results[i,"FrequencySSOverfished"] <- length(which((Biomass[nrow(Biomass),] < BMSY*0.5)==TRUE))
+    ###########
+    
+    ## Calculate frequency of aggregate group collapse (below 100 metric tons) in the last model year
+    # Calculate biomass of aggregate groups for last model year
+    PiscivoresBio <- sum(Biomass[nrow(Biomass),c(1,5)])
+    BenthivoresBio <- sum(Biomass[nrow(Biomass),c(2,8,9)])
+    PlanktivoresBio <- sum(Biomass[nrow(Biomass),c(3,4)])
+    ElasmobranchsBio <- sum(Biomass[nrow(Biomass),c(6,7)])
+    
+    AggregateBios <- list(PiscivoresBio,BenthivoresBio,PlanktivoresBio,ElasmobranchsBio)
+    
+    ## Frequency of aggregate group collapse (below 100mt) in last model year
+    Results[i, "FrequencyAggregateCollapse"] <- length(which((AggregateBios < 100)==TRUE))
+    ###########
+    
+    ## Calculate Total Aggregate Catch for last model year 
+    PiscivoresCat <- sum(Catch[nrow(Catch),c(1,5)])
+    BenthivoresCat <- sum(Catch[nrow(Catch),c(2,8,9)])
+    PlanktivoresCat <- sum(Catch[nrow(Catch),c(3,4)])
+    ElasmobranchsCat <- sum(Catch[nrow(Catch),c(6,7)])
+    
+    Results[i,"PiscivoreCatch"] <- PiscivoresCat
+    Results[i,"BenthivoreCatch"] <- BenthivoresCat
+    Results[i,"PlanktivoreCatch"] <- PlanktivoresCat
+    Results[i,"ElasmobranchCatch"] <- ElasmobranchsCat
+    #############
+    
+    ## Calculate frequency of total system biomass collapse (below 100 metric tons) in last model year
+    Results[i, "SystemCollapse"] <- sum(Biomass[nrow(Biomass),]) < 100
+    ############
+    
+    ##  Total System Biomass in last model year
+    AnnualTotSystemBio <- sum(Biomass[nrow(Biomass),])
+    Results[i,"SystemBiomass"] <- AnnualTotSystemBio
+    ############
+    
+    ## Total System Catch Removal in last model year
+    AnnualTotSystemCat <- sum(Catch[nrow(Catch),])
+    Results[i,"SystemCatch"] <- AnnualTotSystemCat
+    ############
+    
+    ## Biomass diversity in last model year
+    BiomassDiversity <- diversity(Biomass[nrow(Biomass),], index="shannon")
+    Results[i,"BiomassDiversity"] <- BiomassDiversity
+    ############
+    
+    ## Catch diversity in last model year
+    CatchDiversity <-  diversity(Catch[nrow(Catch),], index="shannon")
+    Results[i,"CatchDiversity"] <- CatchDiversity
+    ###########
+    
+    #### Format Explanatory Variable Data for simulation i
+    
+    ## Catch Ceiling
+    Results[i,"CatchCeiling"] <- CeilingValue
+    #########
+    
+    ## Reference Value Data
+    for(isim in 1:length(dat["refvals"][[1]][[1]])){
+      Results[i,paste("RefVal", isim, sep="")] <- dat["refvals"][[1]][[i]][[isim]]
+    }
+    
+    # Limit Value Data
+    for(isim in 1:length(dat["limvals"][[1]][[1]])){
+      Results[i,paste("LimVal", isim, sep="")] <- dat["limvals"][[1]][[i]][[isim]]
+    }
+    #########
+  }
+  return(Results=Results)
+}
+
+
+
+###################################### TreeAnalysis #######################################################################################################
+
+# The TreeAnalysis function runs tree analysis for each performance metric (response variable) against all explanatory variables
+# If response variable is categorical (TRUE value in AsFactor argument) a classification tree rather than a regression tree is produced
+
+# DataFile should contain a data.frame of response (listed first) and explanatory variables
+# NPerformMetrics is the number of performance metrics (number of columns containing response variable data)
+# AsFactor is a list of True/False values that determine if response variable is treated as a factor (categorical)
+# SeedNumber fixes the analysis so results can be replicated
+
+TreeAnalysis <- function(DataFile=NULL,NPerformMetrics=NULL, AsFactor=NULL, SeedNumber=1){
+  ##### Run regression tree #####
+  # Read in Formatted Data
+  Data <- read.table(DataFile)
+  # Load programs
+  library(rpart)
+  # Set up storage for trees and pruning cross validation
+  TreeResults <- list()
+  CPResult <- list()
+  OptimalSplits <- list()
+  OptimalCP <- list()
+  OptimalTreeResults <- list()
+  OptimalTreeVar <- list()
+  OptimalTreeVarImport <- list()
+  
+  # Define list of Performance Metrics
+  PerformMet <- colnames(Data[1:NPerformMetrics])
+  
+  for(i in 1:NPerformMetrics){
+    tryCatch({
+      
+      set.seed(SeedNumber) # plots same tree every time, may change SeedNumber argument to vary results
+      
+      ####################### Produce Initial Tree ###########################################################################
+      if(AsFactor[[i]]==TRUE){
+        Tree <- rpart(as.factor(Data[,i]) ~ as.factor(CatchCeiling) + # response is treated as.factor when data is true/false, or categorical rather than continuous
+                        RefVal1 + RefVal2 + RefVal3 + RefVal4 + 
+                        RefVal5 + RefVal6 + RefVal7 + RefVal8 +
+                        LimVal1 + LimVal2 + LimVal3 + LimVal4 +
+                        LimVal5 + LimVal6 + LimVal7 + LimVal8,
+                      data = Data,
+                      method="class",
+                      control = rpart.control(cp=0.001))
+      } else{
+        Tree <- rpart(Data[,i] ~ as.factor(CatchCeiling) + 
+                        RefVal1 + RefVal2 + RefVal3 + RefVal4 + 
+                        RefVal5 + RefVal6 + RefVal7 + RefVal8 +
+                        LimVal1 + LimVal2 + LimVal3 + LimVal4 +
+                        LimVal5 + LimVal6 + LimVal7 + LimVal8,
+                      data = Data,
+                      method="anova",
+                      control = rpart.control(cp=0.001)) # cp=complexity parameter, splits that don't decrease lack of fit by 0.001 not attempted
+      }
+      
+      # Produce tree graphic
+      par(xpd = NA, mar = c(2.5, 5, 2.5, 5)) # sets up graphing space so no labels are cut off
+      plot(Tree,  main=paste(PerformMet[i],sep=""))
+      text(Tree, cex = 1)
+      par(xpd = F, mar = c(4.5, 4.5, 0.5, 0.5)) # restricts plot to space it is in
+      
+      # Store Tree
+      TreeResults[[i]] <- Tree
+      
+      ################### Plot variety of cp (different tree complexities) and pick optimal complexity###########################################
+      par(mar = c(4.5, 4.5, 2.5, 0.5))
+      plotcp(Tree)
+      # Store pruning cross validation output
+      CPResult[[i]] <- Tree$cptable
+      par(mar = c(4.5, 4.5, 0.5, 0.5))
+      
+      # Pick optimal complexity (leftmost cp which is within 1 std error of the minimum error (the plotted line))
+      MinError <-min(rowSums(CPResult[[i]][,c("xerror","xstd")])) # minimum error represents the line drawn when plotcp() is run
+      PickCP <- min(which(CPResult[[i]][,"xerror"] < MinError)) # gives position of xerrors that meet are less than MinError, pick the minimum position (the leftmost cp on graph that is below dotted line(MinError))
+      OptimalSplits[[i]] <- CPResult[[i]][PickCP,"nsplit"]
+      OptimalCP[[i]] <- CPResult[[i]][PickCP,"CP"]
+      
+      ############# Update to produce Optimal Tree (optimal complexity)############################
+      # Add minsplit to rpart() to force optimal number of splits
+      if(AsFactor[[i]]==TRUE){
+        OptimalTree <- rpart(as.factor(Data[,i]) ~ as.factor(CatchCeiling) + 
+                               RefVal1 + RefVal2 + RefVal3 + RefVal4 + 
+                               RefVal5 + RefVal6 + RefVal7 + RefVal8 + 
+                               LimVal1 + LimVal2 + LimVal3 + LimVal4 +
+                               LimVal5 + LimVal6 + LimVal7 + LimVal8,
+                             data = Data,
+                             method = "class",
+                             control = rpart.control(cp=OptimalCP[[i]])) 
+      } else{
+        OptimalTree <- rpart(Data[,i] ~ as.factor(CatchCeiling) + 
+                               RefVal1 + RefVal2 + RefVal3 + RefVal4 + 
+                               RefVal5 + RefVal6 + RefVal7 + RefVal8 + 
+                               LimVal1 + LimVal2 + LimVal3 + LimVal4 +
+                               LimVal5 + LimVal6 + LimVal7 + LimVal8,
+                             data = Data,
+                             method = "anova",
+                             control = rpart.control(cp=OptimalCP[[i]])) 
+      }
+      
+      # cp=complexity parameter, splits that don't decrease lack of fit by 0.001 not attempted
+      # minslplit set to optimal value determined using TreeAnalysis
+      
+      # Store variables used
+      FrameVars <- OptimalTree$frame[,"var"]
+      Leaves <- FrameVars=="<leaf>"
+      OptimalTreeVar[[i]] <- unique(FrameVars[!Leaves])
+      
+      # Store variable importance
+      OptimalTreeVarImport[[i]] <- OptimalTree$variable.importance
+      
+      # Store optimal tree
+      OptimalTreeResults [[i]] <- OptimalTree
+      
+      #OptimalTree
+      # Produce tree graphic
+      par(xpd = NA, mar = c(2.5, 5, 2.5, 5))
+      plot(OptimalTree, main=paste("Optimal", PerformMet[i],sep=""))
+      text(OptimalTree, cex = 1)
+      par(xpd = F, mar = c(4.5, 4.5, 0.5, 0.5))
+    },
+    error=function(e){
+      print("fit is not a tree, just a root") # e is a conditional object created by the code to store the error
+    })
+  }
+  
+  capture.output(print(TreeResults), file="TreeResults")
+  capture.output(print(CPResult), file="TreeCPResults")
+  capture.output(print(OptimalTreeResults), file="OptimalTreeResults")
+  capture.output(print(OptimalSplits), file="OptimalTreeSplits")
+  capture.output(print(OptimalCP), file="OptimalTreeCP")
+  capture.output(print(OptimalTreeVar), file="OptimalTreeVariables")
+  capture.output(print(OptimalTreeVarImport), file="OptimalTreeVariableImportance")
+}
+
+
+
+###################################### PlotPerfMet #######################################################################################################
+# This function makes boxplots of each performance metric under the different ceiling levels 
+# It is not possible to plot vertical lines between boxplots to show the first split for each performance metric's tree, but it may be desirable to add these mannually to the final plot
+# DataFile should be the same as that used by TreeAnalysis (Produced by FormatTreeAnalysisData)
+# PlotMatrix provides the details of how plots should be ordered graphically
+
+PlotPerfMet <- function(DataFile=NULL, NPerformMetrics=NULL, PlotMatrix=matrix(1,1,byrow=TRUE)){
+  # Read in data
+  Data <- read.table(DataFile)
+  
+  # Define list of Performance Metrics
+  PerformMet <- colnames(Data[1:NPerformMetrics])
+  print(PerformMet)
+  # Determine layout for plots, this is passed to the function as PlotMatrix
+  layout(PlotMatrix)
+  
+  for(i in 1:NPerformMetrics){
+    Plots <- boxplot(formula=(Data[,i]~as.factor(CatchCeiling)), data=Data, ylab=paste(PerformMet[i],sep=""), xlab="Catch ceiling (mt)", cex.lab=1.5, cex.axis=1.5, boxwex=0.75)
+  }
+}
+
+
+
+
+
