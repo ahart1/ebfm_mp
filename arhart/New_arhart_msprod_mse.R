ebc1f7600a0ec066b9d70dd76ab98f2034733bae
ahart1 <arhart555@gmail.com>
2017-05-09 17:36:36 -0400

    I broke up the way tha BMSY is read in, added notes to revise theguilds, and identified Predators and Pelagics for use in calculating  indicators

--- arhart/New_arhart_msprod_mse.R
+++ arhart/New_arhart_msprod_mse.R
@@ -59,7 +59,7 @@ Run <- function()
 {
   # Read in data files, this location must be changed when running on a new device, values from data file assigned to parameters below
   # Read in BMSY and inits data
-  BMSYData <- read.csv("/Users/ahart2/Research/ebfm_mp/data/Bmsy.csv", header=TRUE)
+  BMSYData <- read.csv("/Users/ahart2/Research/ebfm_mp/data/Bmsy.csv", header=TRUE) # column1 is species name, column2 is Bmsy, column3 is mean trophic level
   InitsData <- read.csv("/Users/ahart2/Research/ebfm_mp/data/inits.csv", header=TRUE)
   IndicatorRefVals <- read.csv("/Users/ahart2/Research/ebfm_mp/data/indicator_refvals.csv", header=TRUE)
   # datfile variable contains the file name, reads from json file
@@ -102,10 +102,13 @@ Run <- function()
   hrate <- rep(0,Nsp)
   
   # Define BMSY and pick the species to include in the model
-  BMSY <- BMSYData # column1 is species name, column2 is Bmsy, column3 is mean trophic level
-  BMSY <- BMSY[c(4,5,21,22,14,23,24,6,3,7),]
-  # Set values for BMSY
-  BMSY[,2] <- KGuild/2
+  ModelSpecies <- BMSYData[c(4,5,21,22,14,23,24,6,3,7),1] # pick species names
+  MeanTrophicLevel <- BMSYData[c(4,5,21,22,14,23,24,6,3,7),3] # ID mean trophic level for chosen species
+  BMSY <- KGuild/2 # Set values for BMSY
+  
+  # Identify columns containing predator species
+  Predators <- which(colSums(alpha)>0)
+  
   
   #initial biomass for each species
   Nabund <- Initvals
@@ -120,8 +123,10 @@ Run <- function()
   CI <- dat$CI
   
   # Redefine functional groups
-  theguilds <- c(1,1,2,2,1,3,3,1,1,1)
+  theguilds <- c(1,1,2,2,1,3,3,1,1,1)  ##### ???????????????? this is very error prone, is there a way to get this information from BMSY above or one of the other documents, if yes then change the code for Pelagics below
   
+  # Identify columns containing pelagic species
+  Pelagics <- which(theguilds==2)
   
   ##############################################################################
   # RUN MSE WITH SINGLE SPECIES ASSESSMENT
@@ -164,8 +169,17 @@ Run <- function()
       #Year 1 of model-initial values
       ###################################################################################################
       
+      
+      # ??????????????????? I would like all arguments for eco.indicators to be specified as part of the model's initial conditions,
+      # ??????????????????? I think that initial conditions should be formatted separately and passed to this model in a specific format
+      # ??????????????????? is.predator and is.pelagic should not be calculated here, they should be calculated above
+      
+      
+      
+      
       ############## calculate values for ecological indicators at start of projection#####################
-      eco.results <- eco.indicators(Biomass=NI,Catch=CI,BMSY=KGuild,trophic.level=BMSY[,3],is.predator=which(colSums(alpha)>0),is.pelagic=which(theguilds==2))
+      # ????????????????? why not BMSY=KGuild/2=BMSY as defined earlier, what does NCV stand for in eco.indicators script?
+      eco.results <- eco.indicators(Historic=TRUE, Biomass=NI,Catch=CI,BMSY=KGuild,trophic.level=MeanTrophicLevel,is.predator=Predators,is.pelagic=Pelagics)
       indicators <- eco.results$indicators
       ei.hist <- eco.results$ei.last
       ######################## set initial values for operating model biomass##################################
@@ -253,10 +267,10 @@ Run <- function()
         ############# Update indicators and status
         
         # Calculate ecological indicators based on new data at this time step
-        eco.results <- eco.indicators (Biomass=NI.obs, Catch=CI.obs,BMSY=KGuild,trophic.level=BMSY[,3],is.predator=which(colSums(alpha)>0),is.pelagic=which(theguilds==2))
+        eco.results <- eco.indicators(Historic=FALSE, Biomass=NI.obs, Catch=CI.obs,BMSY=KGuild,trophic.level=MeanTrophicLevel,is.predator=Predators,is.pelagic=Pelagics)
         # ????????does update of indicators just rerun calculation after new data is added (doesn't just use the last year of data)
         ei.now <- eco.results$ei.last
-        indicators <- eco.results$indicators # give matrix of indicator values
+        indicators <- rbind(indicators,eco.results$indicators) # Append new indicator values to indicators dataframe
         #why bother with indicators if only ei.now is referenced and ei.now=indicators???????????????
         
         # Work out status relative to refernce points given new indicators(ei.now)
