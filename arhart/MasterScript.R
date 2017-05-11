6757591ed7a61c4a91bd8a7237730a397e6f6e15
ahart1 <arhart555@gmail.com>
2017-05-11 18:36:39 -0400

    This will eventually be the script that formats the available data, and runs the model

---
+++ arhart/MasterScript.R
@@ -0,0 +1,22 @@
+
+
+# want initial info:
+BMSY
+MTL
+Species names
+
+
+
+
+
+
+# Nyr = defines the number of years that will be projected forward by the operating model
+Nyr=30
+
+
+function(Nsim=NULL, Nyr=NULL)
+
+
+
+
+
