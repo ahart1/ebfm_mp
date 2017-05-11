daf98b1e1df2269ab23c21db329773b063b88667
ahart1 <arhart555@gmail.com>
2017-05-11 18:32:22 -0400

    Minor changes, still not functioning

--- arhart/NewIndicatorRefPtCalcs.R
+++ arhart/NewIndicatorRefPtCalcs.R
@@ -1,27 +1,9 @@
 # New Indicator calculations
 
-##########This script defines the function calc.indicator.hcr and get.refpts###########################
-# A general outline for the project, management options, and performance measures are listed below the two functions
 
-
-#Function (unamed argument without default, unamed argument without default, named argument with default, named argument with default, unamed argument without default)
-#preferably group unamed arguments without defaults together when writing code
-#if no refvals or limvals data is being passed to this function NULL should be written for those arguments??????????
-
-# Where calc.indicator.hcr used, get.fmults=TRUE in original script
 # This function calculates the F-multiplier to be applied across all species
-calc.indicator.hcr <- function(refvals=NULL,limvals=NULL,use.defaults=TRUE, RefFile=NULL, IndicatorValues=NULL, Nsp=NULL) # ??? this comment should be deleted later: there will be 1 indicator value for each indicator used, each is named, not for each species
-{
-  # I think this default may only apply to get.refpts below, if so the next few lines can be deleted??????
-  # This section of code uses default refvalue and limvalues passed to the model in the initial conditions to calculate 
-  if (use.defaults==TRUE)
-  {
-    refvals <- RefFile[,"Threshold"]
-    limvals <- RefFile[,"Limit"]
-    names(refvals) = RefFile[,"Indicator"]
-    names(limvals) = RefFile[,"Indicator"]
-  } else{
-  
+calc.indicator.hcr <- function(refvals=NULL,limvals=NULL, RefFile=NULL, IndicatorValues=NULL, Nsp=NULL){ # ??? this comment should be deleted later: there will be 1 indicator value for each indicator used, each is named, not for each species
+
   # This section compares indicators in this time step to reference and limit values to calculate the F-multiplier used in harvest control rules
   # The calculated F-multiplier is applied to all species
   fmult <- matrix(NA, nrow=length(refvals), ncol=Nsp)
@@ -38,7 +20,7 @@ calc.indicator.hcr <- function(refvals=NULL,limvals=NULL,use.defaults=TRUE, RefF
 
   temp[IndicatorValues[UseRefvalsLarger]<refvals[UseRefvalsLarger] && IndicatorValues[UseRefvalsLarger]>=limvals[UseRefvalsLarger]] <- (IndicatorValues[UseRefvalsLarger]-limvals[UseRefvalsLarger])/(refvals[UseRefvalsLarger]-limvals[UseRefvalsLarger]) # for indicator values between refvals and limvals
   
-  fmult[UseRefvalsLarger,] <- -1*temp*as.numeric(RefFile[UseRefvalsLarger,6:15]) # ??? where is ind.hcr coming from?  ???Test that the multiplication is ocurring across the correct rows of ind.hcr 
+  fmult[UseRefvalsLarger,] <- -1*temp*as.numeric(RefFile[UseRefvalsLarger,6:15]) #  ???Test that the multiplication is ocurring across the correct rows of RefFile
   
   #### Indicators with limvals greater than refvals ####
   LimvalsLarger <- names(which(refvals<limvals))
@@ -50,8 +32,8 @@ calc.indicator.hcr <- function(refvals=NULL,limvals=NULL,use.defaults=TRUE, RefF
   
   temp[IndicatorValues[UseLimvalsLarger]>refvals[UseLimvalsLarger] && IndicatorValues[UseLimvalsLarger]<=limvals[UseLimvalsLarger]] <- (IndicatorValues[UseLimvalsLarger]-limvals[UseLimvalsLarger])/(refvals[UseLimvalsLarger]-limvals[UseLimvalsLarger]) # indicator less than/=limval or greater than refval
   
-  fmult[UseLimvalsLarger,] <- -1*temp*as.numeric(RefFile[UseLimvalsLarger,6:15]) # I may have renamed ind.hcr?????????
-  }
+  fmult[UseLimvalsLarger,] <- -1*temp*as.numeric(RefFile[UseLimvalsLarger,6:15])
+  
   
   fmult[which(is.na(fmult)==TRUE)] <- 1 # Assign missing (NA) values 1, no change in F
   return(fmult)
@@ -65,74 +47,91 @@ calc.indicator.hcr <- function(refvals=NULL,limvals=NULL,use.defaults=TRUE, RefF
 
 ## ???????????????? I probably want the below to pick values from a vector as in PickIndicators rather than running a for() loop
 
-# For each indicator get.refpts() calculates reference and limit values used in indicator-based harvest control rules and returns refpts
-get.refpts <- function(use.defaults=TRUE, RefFile=NULL, IndicatorValues=NULL){
+# For each indicator get.refpts() calculates reference and limit values for all possible indicators that may be used in indicator-based harvest control rules and returns refpts 
+get.refpts <- function(use.defaults=TRUE, RefFile=NULL){
   
-  # This section of code uses default values passed to the model in the initial conditions
+  # This section of code uses default values passed to the model in the initial conditions, it is mostly to test performance against known refvals and limvals
   if (use.defaults==TRUE)
   {
     refvals <- RefFile[,"Threshold"]
     limvals <- RefFile[,"Limit"]
     names(refvals) = RefFile[,"Indicator"]
     names(limvals) = RefFile[,"Indicator"]
-  }  
-  # This section of code calculates new refvals and limvals for refpts passed to model in the initial conditions ??????
-   #Calculate refpoints
-  
-  # ?????? NO NO NO NO NO NO NO
-  bounds <- matrix(c(6,3,6,3,1,0,0,1,0,1,0,1,0,1,20,0),ncol=2,byrow=TRUE) # ?????? NO, this information should be passed in to initial model conditions it could easily be included in IndicatorRefVals file!!!!
-  rownames(bounds) = RefFile[,"Indicator"]
-  # ????? NO NO NO NO NO NO NO
-  refvals <- rep(NA,nrow(RefFile[,"Indicator"]))
-  limvals <- rep(NA,nrow(RefFile[,"Indicator"]))
- 
-  
-  # Run the following to ask questions below: ???
-  bounds <- matrix(c(6,3,6,3,1,0,0,1,0,1,0,1,0,1,20,0),ncol=2,byrow=TRUE)
-  rownames(bounds) = IndicatorRefVals[,"Indicator"]
-  bounds
-  # end of example, delete in final code???????? Calculate initial indicator refvals and limvals based on name not number!!!!!!
-  
-  for (i in 1:nrow(bounds))
-  {
-    flag=0
-    lo <- which.min(bounds[i,]) # ID lower bound
-    hi <- which.max(bounds[i,]) # ID upper bound
-    if (bounds[i,2]>bounds[i,1]) # if upper bound is in second column flag=1
-    {
-      flag=1
-    }
+  }else{  
+    # This section of code calculates new refvals and limvals rather than using defaults
+    refvals <- rep(NA,nrow(RefFile))
+    limvals <- rep(NA,nrow(RefFile))
+    names(refvals) = RefFile[,"Indicator"] #these names should be asigned as they go not at the end to make sure that name corresponds to calculation
+    names(limvals) = RefFile[,"Indicator"]
     
-    if (i==3)
+    # Read in bounds from RefFile 
+    # Bound1 <- RefFile[,"Bound1]
+    # Bound2 <- RefFile[,"Bound2]
+    # Bounds <- cbind(Bound1, Bound2)
+    # Lower <- NULL
+    # Upper <- NULL
+    
+    # ID Lower and upper bounds and label
+    # for(i in 1:nrow(RefFile)){
+    # Lower[i] <- which.min(Bounds[i,])
+    # Upper[i] <- which.max(Bounds[i,])
+    # }
+    # names(Lower) <- RefFile[,"Indicator"]
+    # names(Upper) <- RefFile[,"Indicator]
+    
+    # Run the following to ask questions below: ???
+    bounds <- matrix(c(6,3,6,3,1,0,0,1,0,1,0,1,0,1,20,0),ncol=2,byrow=TRUE)
+    rownames(bounds) = IndicatorRefVals[,"Indicator"]
+    bounds
+    # end of example, delete in final code???????? Calculate initial indicator refvals and limvals based on name not number!!!!!!
+    
+    for (i in 1:nrow(RefFile))
     {
-      refvals[i] <- runif(1,bounds[i,2],bounds[i,1]) # calculates 1 value from a uniform distribution with lower bound in second column of row 3, upper bound in 1st column of row 3
-      limvals[i] <- runif(1,refvals[i],bounds[i,1]) # calculates 1 value with lowerbound=refval in row 3, upper bound in first column of row 3
-    }
-    if (i==4)
-    {
-      refvals[i] <- runif(1,bounds[i,1],refvals[i-1])
-      limvals[i] <- runif(1,bounds[i,1],refvals[i])
-    }
-    if (i==5)
-    {
-      refvals[i] <- runif(1,bounds[i,1],bounds[i,2])
-      limvals[i] <- refvals[i]
-    }
-    if (i==6)
-    {
-      refvals[i] <- runif(1,bounds[i,lo],refvals[i-1]) # ???? Why the different inc syntax from previous rows
-      limvals[i] <- refvals[i]
-    }
-    vec <- c(1,2,7,8)
-    if (is.na(match(i,vec))==FALSE)
-    {
-      refvals[i] <- runif(1,bounds[i,lo],bounds[i,hi])
-      if (flag==0) limvals[i] <- runif(1,bounds[i,lo],refvals[i])
-      if (flag==1) limvals[i] <- runif(1,refvals[i],bounds[i,hi])
+      
+      # which((RefFile[,"Bound1"]<RefFile[,"Bound2"])==TRUE)
+      
+      flag=0
+      lo <- which.min(bounds[i,]) # ID lower bound
+      hi <- which.max(bounds[i,]) # ID upper bound
+      if (bounds[i,2]>bounds[i,1]) # if upper bound is in second column flag=1
+      {
+        flag=1
+      }
+      # ????? I don't really understand the above flag stuff still
+      
+      # if(RefFile[,"Indicator"]=="High.prop.pel"){
+      #  refvals["High.prop.pel"] <- runif(1, Bounds["High.prop.pel",Lower["High.prop.pel"]], Bounds["High.prop.pel",Upper["High.prop.pel"]]) # calculates 1 value from a uniform distribution with calculated position of lower bound (column2), calculated position of upper bound (column1)
+      #  limvals["High.prop.pel"] <- runif(1, refvals["High.prop.pel], Bounds["High.prop.pel",Upper["High.prop.pel"]])
+      if (i==3) 
+      {
+        refvals[i] <- runif(1,bounds[i,2],bounds[i,1]) # calculates 1 value from a uniform distribution with lower bound in second column of row 3, upper bound in 1st column of row 3
+        limvals[i] <- runif(1,refvals[i],bounds[i,1]) # calculates 1 value with lowerbound=refval in row 3, upper bound in first column of row 3
+      }
+      if (i==4)
+      {
+        refvals[i] <- runif(1,bounds[i,1],refvals[i-1]) 
+        limvals[i] <- runif(1,bounds[i,1],refvals[i])
+      }
+      if (i==5)
+      {
+        refvals[i] <- runif(1,bounds[i,1],bounds[i,2])
+        limvals[i] <- refvals[i]
+      }
+      if (i==6)
+      {
+        refvals[i] <- runif(1,bounds[i,lo],refvals[i-1]) # ???? Why the different inc syntax from previous rows
+        limvals[i] <- refvals[i]
+      }
+      vec <- c(1,2,7,8)
+      if (is.na(match(i,vec))==FALSE)
+      {
+        refvals[i] <- runif(1,bounds[i,lo],bounds[i,hi])
+        if (flag==0) limvals[i] <- runif(1,bounds[i,lo],refvals[i])
+        if (flag==1) limvals[i] <- runif(1,refvals[i],bounds[i,hi])
+      }
     }
   }
-  names(refvals) = RefFile[,"Indicator"] #these names should be asigned as they go not at the end to make sure that name corresponds to calculation
-  names(limvals) = RefFile[,"Indicator"]
+ 
   refpts <- NULL
   refpts$refvals <- refvals
   refpts$limvals <- limvals
