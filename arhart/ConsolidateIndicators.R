e671d730c8ef6baaec4ee86151c12d5972e9839d
ahart1 <arhart555@gmail.com>
2017-05-11 18:28:32 -0400

    I changed UseInds to UseStatusMeasures to reflect the combination of performance metrics and indicators calculated by these functions. I also moved the PickIndicators function to Initial conditions functions

--- arhart/ConsolidateIndicators.R
+++ arhart/ConsolidateIndicators.R
@@ -4,23 +4,7 @@
 # get.Indicators
 
 
-########## PickIndicators ##########
 
-# This function makes a list of indicators to be used in the simulations
-# PickOption indicates which option should be used (this will allow custom indicator combinations to be specified by creating a new option)
-# PotentialInds = indicators to be considered/picked from, this information comes from the initial information passed to the model
-# This will return a list of indicators by name to be used in the model simulation, order of indicator names does not matter   ?????THIS WORKS I TESTED IT
-
-PickIndicators <- function(PickOption="Option1", PotentialInds=NULL){
-  if(PickOption=="Option1"){
-    IndicatorPicks <- sample(PotentialInds[,"Indicator"], length(PotentialInds[,"Indicator"]), replace=FALSE) #This creates a list of all indicators being considered (samples each possible indicator)
-  }
-  if(PickOption=="Option2"){
-    Nchoose <- sample(1:length(PotentialInds[,"Indicator"]), 1, replace=FALSE) # Pick a random number between 1 and length of PotentialInds to determine the number of indicators that will be used in the simulation
-    IndicatorPicks <- sample(PotentialInds[,"Indicator"], Nchoose, replace=False) # This creates a list containing Nchoose indicators randomly picked from NInds number of possible indicators
-  }
-  return(IndicatorPicks)
-}
 
 ########## eco.indicators ##########
 
@@ -51,141 +35,164 @@ eco.indicators <- function(Historic=TRUE, Biomass=NULL,Catch=NULL,BMSY=NULL,trop
 #This script calculated indicator values for all possible indicators based on historical data (not all of these indicators will be used in each simulation) and stores in a dataframe
 # I think that initially Biomass=NI which has 10 values (1 for each species) for each historic year
 # Historic= indicates whether this function is calculating the historic indicators (TRUE) or updating based on new model simulated data (FALSE)
-# UseInds = Indicators to be calculated for this simulation, calculated using PickIndicators as part of initial model conditions
+# UseStatusMeasures = Indicators and performance metrics to be calculated for this simulation, calculated using PickStatusMeasures as part of initial model conditions
 # ???????? is.predator and is.prelagic need to be a list/vector or species names in "", shouldn't need to be changed if format is as described in New_arhart_msprod_mse
 
 # This function must be passed arguments which are used for indicator calculations, indicators that are not being calculated in a given set of simulations need not be given information since default=NULL
 
-get.Indicators <- function(UseInds=NULL, Historic=TRUE,Biomass=NULL,Catch=NULL,BMSY=NULL,trophic.level=NULL,is.predator=NULL,is.pelagic=NULL,lifespan=NULL,size=NULL){
+get.Indicators <- function(UseStatusMeasures=NULL, Historic=TRUE,Biomass=NULL,Catch=NULL,BMSY=NULL,trophic.level=NULL,is.predator=NULL,is.pelagic=NULL,lifespan=NULL,size=NULL){
   
   if(Historic=TRUE){
-    if("tot.bio"%in%UseInds | "exprate"%in%UseInds | "prop.predators"%in%UseInds | "TL.survey"%in%UseInds | "mean.length"%in%UseInds | "mean.lifespan"%in%UseInds ==TRUE){ # ??? may need to include div.cv.bio??? see below also
-      tot.bio.values <- rowSums(Biomass,na.rm=TRUE) # Total system biomass summed over all species
-      tot.bio <- as.data.frame(tot.bio.values) # Format data as a dataframe
+    #### Performance Metrics ####
+    if("tot.bio"%in%UseStatusMeasures | "exprate"%in%UseStatusMeasures | "Low.prop.pelagic"%in%UseStatusMeasures | "High.prop.pelagic"%in%UseStatusMeasures | "Low.prop.predators"%in%UseStatusMeasures | "High.prop.predators"%in%UseStatusMeasures | "TL.survey"%in%UseStatusMeasures | "mean.length"%in%UseStatusMeasures | "mean.lifespan"%in%UseStatusMeasures ==TRUE){ # ??? may need to include div.cv.bio??? see below also
+      tot.bio <-  rowSums(Biomass,na.rm=TRUE) # Total system biomass summed over all species
     }
-    if("tot.cat"%in%UseInds | "exprate"%in%UseInds | "TL.landings"%in%UseInds==TRUE){
-      tot.cat.values <- rowSums(Catch,na.rm=TRUE) # Total system catch summed over all species
-      tot.cat <- as.data.frame(tot.cat.values)
+    if("tot.cat"%in%UseStatusMeasures | "exprate"%in%UseStatusMeasures | "TL.landings"%in%UseStatusMeasures==TRUE){
+      tot.cat <- rowSums(Catch,na.rm=TRUE) # Total system catch summed over all species
     }
-    if("exprate" %in% UseInds==TRUE){
-      exprate.values <- tot.cat/tot.bio # Exploitation rate
-      exprate <- as.data.frame(exprate.values)
+    if("exprate" %in% UseStatusMeasures==TRUE){
+      exprate <- tot.cat/tot.bio # Exploitation rate
     }
-    if("prop.predators" %in% UseInds==TRUE){
-      prop.predators.values <- rowSums(Biomass[,is.predator],na.rm=TRUE)/tot.bio # Proportion of total biomass that is comprised by predatory species
-      prop.predators <- as.data.frame(prop.predators.values)
+    if("pd.ratio" %in% UseStatusMeasures==TRUE){
+      pd.ratio <- rowSums(Biomass[,is.pelagic],na.rm=TRUE)/rowSums(Biomass[,-(is.pelagic)],na.rm=TRUE) # Pelagic demersal ratio of biomass
     }
-    if("pd.ratio" %in% UseInds==TRUE){
-      pd.ratio.values <- rowSums(Biomass[,is.pelagic],na.rm=TRUE)/rowSums(Biomass[,-(is.pelagic)],na.rm=TRUE) # Pelagic demersal ratio
-      pd.ratio <- as.data.frame(pd.ratio.values)
+    
+    #### Indicators for control rules ####
+    if("Low.prop.predators" %in% UseStatusMeasures==TRUE){
+      Low.prop.predators <- rowSums(Biomass[,is.predator],na.rm=TRUE)/tot.bio # Proportion of total biomass that is comprised by predatory species
     }
-    if("prop.pelagic" %in% UseInds==TRUE){
-      prop.pel.values <- 1-(1/(pd.ratio+1)) # Proportion of total biomass that is made of pelagic species ????? I probably need to change the %in% statement so that pd.ratio is claculated when this metric is used, see below also
-      prop.pelagic <- as.data.frame(prop.pel.values)
+    if("High.prop.predators" %in% UseStatusMeasures==TRUE){
+      High.prop.predators <- rowSums(Biomass[,is.predator],na.rm=TRUE)/tot.bio # Proportion of total biomass that is comprised by predatory species
     }
-    if("TL.landings" %in% UseInds==TRUE){
+    if("Low.prop.pelagic" %in% UseStatusMeasures==TRUE){
+      Low.prop.pelagic <- rowSums(Biomass[,is.pelagic],na.rm=TRUE)/tot.bio # Proportion of total biomass that is made of pelagic species 
+    }
+    if("High.prop.pelagic" %in% UseStatusMeasures==TRUE){
+      High.prop.pelagic <- rowSums(Biomass[,is.pelagic],na.rm=TRUE)/tot.bio # Proportion of total biomass that is made of pelagic species 
+    }
+    if("TL.landings" %in% UseStatusMeasures==TRUE){
       IndicatorCalcs <- function(X,IndData){
         X*IndData
       }
       tot.cat.TL <- rowSums(mapply(X=Catch, FUN=IndicatorCalcs, IndData=trophic.level), na.rm=TRUE)
       TL.landings <- tot.cat.TL/tot.cat  # Trophic level of landings 
     }
-    if("TL.survey" %in% UseInds==TRUE){
+    if("TL.survey" %in% UseStatusMeasures==TRUE){
       IndicatorCalcs <- function(X,IndData){
         X*IndData
       }
       tot.bio.TL <- rowSums(mapply(X=Biomass, FUN=IndicatorCalcs, IndData=trophic.level), na.rm=TRUE)
       TL.survey <- tot.bio.TL/tot.bio # Trophic level of survey
     }
-    if("prop.overfished" %in% UseInds==TRUE){
+    if("prop.overfished" %in% UseStatusMeasures==TRUE){
       PropOverfishedCalc <- function(X, BMSY){
         length(which(X<0.5*BMSY))/10
       }
       prop.overfished <- apply(X=Biomass, MARGIN=1, FUN=PropOverfishedCalc, BMSY=BMSY)  # Proportion of species that is overfished (less than half BMSY)
     }
-    if("div.cv.bio" %in% UseInds==TRUE){
-      div.cv.bio <- rep(NA,nrow(Biomass)-10)
-      for (i in 10:nrow(Biomass))
-        div.cv.bio[i-9] <- 1/(sd(tot.bio[((i-9):i)],na.rm=TRUE)/mean(tot.bio[((i-9):i)],na.rm=TRUE))
-      # ?????? THIS STILL HAS A FOR() LOOP BUT I DON'T UNDERSTAND IT SO I HAVENT CHANGED ANYTHING BUT IT SHOULD BE FIXED
+    if("div.cv.bio" %in% UseStatusMeasures==TRUE){
+    div.cv.bio <- rep(NA,nrow(Biomass))
+    for (i in 10:nrow(Biomass))
+      div.cv.bio[i] <- 1/(sd(tot.bio[((i-9):i)],na.rm=TRUE)/mean(tot.bio[((i-9):i)],na.rm=TRUE)) # 1/(CV biomass) for last ten years (current model year and previous 9 years)
     }
-    if("mean.length" %in% UseInds==TRUE){
+    if("mean.length" %in% UseStatusMeasures==TRUE){
       tot.mean.length <- rowSums(mapply(X=Biomass, FUN=IndicatorCalcs, IndData=size),na.rm=TRUE)
       mean.length <- tot.mean.length/tot.bio # Average mean length of fish across all biomass
     }
-    if("mean.lifespan" %in% UseInds==TRUE){
+    if("mean.lifespan" %in% UseStatusMeasures==TRUE){
       tot.mean.lifespan <- rowSums(mapply(X=Biomass, FUN=IndicatorCalcs, IndData=length),na.rm=TRUE)
       mean.lifespan <- tot.mean.lifespan/tot.bio # Average mean lifespan of fish across all biomass 
     }
     
-    IndicatorValues <- cbind(tot.bio, tot.cat, exprate, TL.landings, TL.survey, prop.predators, pd.ratio, prop.pelagic, prop.overfished)
-    return(IndicatorValues)
+    StatusMeasuredValues <- NULL
+    
+    IndicatorValueList <- list(TL.landings, TL.survey, prop.predators, prop.pelagic, prop.overfished) # ??? This will not work unless all are calculated, I need a way to make a list of objects based on those chosen for chosen indicators ?????
+    StatusMeasuredValues$Indicators <- as.data.frame(IndicatorValueList)
+    
+    PerformMetricValueList <- list(tot.bio, tot.cat, exprate, pd.ratio)
+    StatusMeasuredValues$PerformMetric <- as.data.frame(PerformMetricValueLis)
+
+    return(StatusMeasuredValues)
+    
+    
+    #??????? fix this
+    refpts <- NULL
+    refpts$refvals <- refvals
+    refpts$limvals <- limvals
+    return(refpts)
   }
   
   if(Historic=FALSE){
-    if("tot.bio"%in%UseInds | "exprate"%in%UseInds | "prop.predators"%in%UseInds | "TL.survey"%in%UseInds | "mean.length"%in%UseInds | "mean.lifespan"%in%UseInds ==TRUE){
-      tot.bio.values <- rowSums(Biomass[nrow(Biomass),],na.rm=TRUE) # Total system biomass summed over all species for most recent year (last row of matrix)
-      tot.bio <- as.data.frame(tot.bio.values) # Format data as a dataframe 
+    #### Performance Metrics ####
+    if("tot.bio"%in%UseStatusMeasures | "exprate"%in%UseStatusMeasures | "Low.prop.pelagic"%in%UseStatusMeasures | "High.prop.pelagic"%in%UseStatusMeasures | "Low.prop.predators"%in%UseStatusMeasures | "High.prop.predators"%in%UseStatusMeasures | "TL.survey"%in%UseStatusMeasures | "mean.length"%in%UseStatusMeasures | "mean.lifespan"%in%UseStatusMeasures ==TRUE){
+      tot.bio <- rowSums(Biomass[nrow(Biomass),],na.rm=TRUE) # Total system biomass summed over all species for most recent year (last row of matrix)
     }
-    if("tot.cat"%in%UseInds | "exprate"%in%UseInds | "TL.landings"%in%UseInds==TRUE){
-      tot.cat.values <- rowSums(Catch[nrow(Catch),],na.rm=TRUE) # Total system catch summed over all species for most recent year
-      tot.cat <- as.data.frame(tot.cat.values)
+    if("tot.cat"%in%UseStatusMeasures | "exprate"%in%UseStatusMeasures | "TL.landings"%in%UseStatusMeasures==TRUE){
+      tot.cat <- rowSums(Catch[nrow(Catch),],na.rm=TRUE) # Total system catch summed over all species for most recent year
     }
-    if("exprate" %in% UseInds==TRUE){
-      exprate.values <- tot.cat/tot.bio # Exploitation rate
-      exprate <- as.data.frame(exprate.values)
+    if("exprate" %in% UseStatusMeasures==TRUE){
+      exprate <- tot.cat/tot.bio # Exploitation rate
     }
-    if("prop.predators" %in% UseInds==TRUE){
-      prop.predators.values <- rowSums(Biomass[nrow(Biomass),is.predator],na.rm=TRUE)/tot.bio # Proportion of total biomass that is comprised by predatory species
-      prop.predators <- as.data.frame(prop.predators.values)
+    if("pd.ratio" %in% UseStatusMeasures==TRUE){
+      pd.ratio <- rowSums(Biomass[nrow(Biomass),is.pelagic],na.rm=TRUE)/rowSums(Biomass[nrow(Biomass),-(is.pelagic)],na.rm=TRUE)   # Pelagic demersal ratio (Pelagic:Everything not pelagic Ratio)
     }
-    if("pd.ratio" %in% UseInds==TRUE){
-      pd.ratio.values <- rowSums(Biomass[nrow(Biomass),is.pelagic],na.rm=TRUE)/rowSums(Biomass[nrow(Biomass),-(is.pelagic)],na.rm=TRUE)   # Pelagic demersal ratio (Pelagic:Everything not pelagic Ratio)
-      pd.ratio <- as.data.frame(pd.ratio.values) 
+    
+    #### Indicators for control rules ####
+    if("Low.prop.predators" %in% UseStatusMeasures==TRUE){
+      Low.prop.predators <- rowSums(Biomass[nrow(Biomass),is.predator],na.rm=TRUE)/tot.bio # Proportion of total biomass that is comprised by predatory species
     }
-    if("prop.pelagic" %in% UseInds==TRUE){
-      #?????????????? THIS HASN'T BEEN ALTERED TO ONLY CALCULATE LAST YEAR
-      prop.pel.values <- 1-(1/(pd.ratio+1))           # Proportion of total biomass that is made of pelagic species
-      prop.pelagic <- as.data.frame(prop.pel.values)
+    if("High.prop.predators" %in% UseStatusMeasures==TRUE){
+      High.prop.predators <- rowSums(Biomass[nrow(Biomass),is.predator],na.rm=TRUE)/tot.bio # Proportion of total biomass that is comprised by predatory species
     }
-    if("TL.landings %in% UseInds==TRUE"){
+    if("Low.prop.pelagic" %in% UseStatusMeasures==TRUE){
+      Low.prop.pelagic <- rowSums(Biomass[nrow(Biomass),is.pelagic],na.rm=TRUE)/tot.bio  # Proportion of total biomass that is made of pelagic species
+    }
+    if("High.prop.pelagic" %in% UseStatusMeasures==TRUE){
+      High.prop.pelagic <- rowSums(Biomass[nrow(Biomass),is.pelagic],na.rm=TRUE)/tot.bio  # Proportion of total biomass that is made of pelagic species
+    }
+    if("TL.landings %in% UseStatusMeasures==TRUE"){
       IndicatorCalcs <- function(X,IndData){
         X*IndData
       }
       tot.cat.TL <- rowSums(mapply(X=Catch[nrow(Catch),], FUN=IndicatorCalcs, IndData=trophic.level), na.rm=TRUE)
       TL.landings <- tot.cat.TL/tot.cat  # Trophic level of landings
     }
-    if("TL.survey" %in% UseInds==TRUE){
+    if("TL.survey" %in% UseStatusMeasures==TRUE){
       IndicatorCalcs <- function(X,IndData){
         X*IndData
       }
       tot.bio.TL <- rowSums(mapply(X=Biomass[nrow(Biomass),], FUN=IndicatorCalcs, IndData=trophic.level), na.rm=TRUE)
       TL.survey <- tot.bio.TL/tot.bio    # Trophic level of survey
     }
-    if("prop.overfished" %in% UseInds==TRUE){
+    if("prop.overfished" %in% UseStatusMeasures==TRUE){
       PropOverfishedCalc <- function(X, BMSY){
         length(which(X<0.5*BMSY))/10
       }
       prop.overfished <- apply(X=Biomass[nrow(Biomass),], MARGIN=1, FUN=PropOverfishedCalc, BMSY=BMSY)  # Proportion of species that is overfished (less than half BMSY)
     }
-    if("div.cv.bio" %in% UseInds==TRUE){
-      #?????????????? THIS HASN'T BEEN ALTERED TO ONLY CALCULATE LAST YEAR
-      div.cv.bio <- rep(NA,nrow(Biomass)-10)
-      for (i in 10:nrow(Biomass))
-        div.cv.bio[i-9] <- 1/(sd(tot.bio[((i-9):i)],na.rm=TRUE)/mean(tot.bio[((i-9):i)],na.rm=TRUE))
-      # ?????? THIS STILL HAS A FOR() LOOP BUT I DON'T UNDERSTAND IT SO I HAVENT CHANGED ANYTHING BUT IT SHOULD BE FIXED
+    if("div.cv.bio" %in% UseStatusMeasures==TRUE){
+      div.cv.bio <- 1/(sd(tot.bio[(nrow(Biomass)-9):nrow(Biomass)], na.rm=TRUE)/mean(tot.bio[(nrow(Biomass)-9):nrow(Biomass)], na.rm=TRUE))
     }
-    if("mean.length" %in% UseInds==TRUE){
+    if("mean.length" %in% UseStatusMeasures==TRUE){
       tot.mean.length <- rowSums(mapply(X=Biomass[nrow(Biomass),], FUN=IndicatorCalcs, IndData=size),na.rm=TRUE)
       mean.length <- tot.mean.length/tot.bio # Average mean length of fish across all biomass
     }
-    if("mean.lifespan" %in% UseInds==TRUE){
+    if("mean.lifespan" %in% UseStatusMeasures==TRUE){
       tot.mean.lifespan <- rowSums(mapply(X=Biomass[nrow(Biomass),], FUN=IndicatorCalcs, IndData=length),na.rm=TRUE)
       mean.lifespan <- tot.mean.lifespan/tot.bio # Average mean lifespan of fish across all biomass
     }
-
-    IndicatorValues <- cbind(tot.bio, tot.cat, exprate, TL.landings, TL.survey, prop.predators, pd.ratio, prop.pelagic, prop.overfished)
-    return(IndicatorValues)
+    
+    # ??????? IndicatorValues <- cbind(tot.bio, tot.cat, exprate, TL.landings, TL.survey, prop.predators, pd.ratio, prop.pelagic, prop.overfished)
+    StatusMeasuredValues <- NULL
+    
+    IndicatorValueList <- list(TL.landings, TL.survey, prop.predators, prop.pelagic, prop.overfished) # ??? This will not work unless all are calculated, I need a way to make a list of objects based on those chosen for chosen indicators ?????
+    StatusMeasuredValues$Indicators <- as.data.frame(IndicatorValueList)
+    
+    PerformMetricValueList <- list(tot.bio, tot.cat, exprate, pd.ratio)
+    StatusMeasuredValues$PerformMetric <- as.data.frame(PerformMetricValueLis)
+    
+    return(StatusMeasuredValues)
+    
   }
 }
 
