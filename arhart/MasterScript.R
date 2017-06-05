38ffba8e09360c441da480b0723e6107ae3c0afb
ahart1 <arhart555@gmail.com>
2017-06-05 18:25:07 -0400

    This is the script that will be used to run the model, the first part of the file defines arguments for the function, then the function is called, then there are a bunch of arguments that don't currently appear to be used?

--- arhart/MasterScript.R
+++ arhart/MasterScript.R
@@ -1,45 +1,72 @@
 
-
-# want initial info:
-BMSY
-MTL
-Species names
-
-
-
-
-
-
-# Nyr = defines the number of years that will be projected forward by the operating model
-Nyr=30
-#Set number of simulations
-Nsim <- 1000
-
-function(Nsim=NULL, Nyr=NULL)
-
-
-  # Read in data files, this location must be changed when running on a new device, values from data file assigned to parameters below
-  # Read in BMSY and inits data
-  BMSYData <- read.csv("/Users/ahart2/Research/ebfm_mp/data/Bmsy.csv", header=TRUE) # column1 is species name, column2 is Bmsy, column3 is mean trophic level
-InitsData <- read.csv("/Users/ahart2/Research/ebfm_mp/data/inits.csv", header=TRUE)
-IndicatorRefVals <- read.csv("/Users/ahart2/Research/ebfm_mp/data/indicator_refvals.csv", header=TRUE) # Must contain the following columns: Indicator, IndC, Threshold, Limit, T.L, column for each species
+# Read in data files
+BMSYData <- read.csv("/Users/ahart2/Research/ebfm_mp/arhart/DataFiles/FormattedSpeciesBmsy.csv", header=TRUE) # column1 is species name, column2 is Bmsy, column3 is mean trophic level
+InitsData <- read.csv("/Users/ahart2/Research/ebfm_mp/arhart/DataFiles/FormattedInitialSpeciesParameters.csv", header=TRUE)
+IndicatorRefVals <- read.csv("/Users/ahart2/Research/ebfm_mp/arhart/DataFiles/FormattedIndicatorRefLimVals.csv", header=TRUE) # Must contain the following columns: Indicator, IndC, Threshold, Limit, T.L, column for each species
 # datfile variable contains the file name, reads from json file
 datfilename <- "/Users/ahart2/Research/ebfm_mp/data/Georges.dat.json"
 dat <- fromJSON(datfilename)
-  
-  ##############################################################################
-  # Define parameters for use in the model
-  ##############################################################################
-  # Parameters include r, KGuild (Carrying capacity of guild), Ktot (Carrying capacity of total system), Guildmembership, BetweenGuildComp (competition), WithinGuildComp, alpha, hrate(harvest rate) 
-  
-  
+
+# Define BMSY and pick the species to include in the model
+
+
+
+# Arguments required by RunMultiSpeciesProdWithCeiling() function
+   ScriptWorkDir <- "/Users/ahart2/Research/ebfm_mp/arhart"    
+   WorkDir <- "/Users/ahart2/Research/ebfm_mp/arhart"  
+   OUTPUTdir <- "/Users/ahart2/Research/ebfm_mp/arhart/TryRunning"  
+   Nsim <- 3
+   Nyr <- 30
+   SpeciesNames <- as.character(BMSYData[c(4,5,21,22,14,23,24,6,3,7),"Species.Group"]) 
+   # Predators: Vector of species names (strings) for predatory species
+      # alpha is predation
+      alpha <- as.matrix(dat$alpha)
+      colnames(alpha) <- SpeciesNames
+      spatial.overlap <- dat$spatial.overlap
+      alpha <- alpha*spatial.overlap
+      # Identify columns containing predator species
+   Predators <- names(which(colSums(alpha)>0))
+   # Pelagics: Vector of species names (strings) for pelagic species
+      # Redefine functional groups
+      theguilds <- c(1,1,2,2,1,3,3,1,1,1)  ##### ???????????????? this is very error prone, is there a way to get this information from BMSY above or one of the other documents, if yes then change the code for Pelagics below
+      # Identify columns containing pelagic species
+   Pelagics <- which(theguilds==2)
+   PickStatusMeasureOption <- 1
+   StatusMeasures <- c("TL.survey", "TL.landings", "High.prop.pelagic", "Low.prop.pelagic", "High.prop.predators", "Low.prop.predators", "prop.overfished", "div.cv.bio", "tot.bio", "tot.cat", "exprate", "pd.ratio")
+   HistoricBiomass <- dat$NI
+      HistoricBiomass <- HistoricBiomass[,-1]
+      colnames(HistoricBiomass) <- SpeciesNames
+   HistoricCatch <- dat$CI
+      colnames(HistoricCatch) <- SpeciesNames
+   # BMSYData: Vector containing BMSY for each species
+      KGuild <- dat$KGuild # Carrying capacity for each guild, each species is its own guild
+   BMSY <- KGuild/2 # Set values for BMSY
+   MeanTrophicLevel <- BMSYData[c(4,5,21,22,14,23,24,6,3,7),"MTL"] # ID mean trophic level for chosen species, could also ID by species
+   DefaultRefLimVals <- FALSE
+   IndicatorData <- IndicatorRefVals
+   InitialSpeciesData <- InitsData
+   ChooseFMult <- 4   # Choose median F-Multiplier for each species column
+
+   
+# 1 Double check the return values from the function
+# 2 source new script
+# 3 try running the next line
+RunMultiSpeciesProdWithCeiling(ScriptWorkDir=ScriptWorkDir, WorkDir=WorkDir, OUTPUTdir=OUTPUTdir, Nsim=Nsim, Nyr=Nyr, SpeciesNames=SpeciesNames, Predators=Predators, Pelagics=Pelagics, StatusMeasures=StatusMeasures, 
+                               HistoricBiomass=HistoricBiomass, HistoricCatch=HistoricCatch, BMSYData=BMSYData, MeanTrophicLevel=MeanTrophicLevel, DefaultRefLimVals=DefaultRefLimVals, IndicatorData=IndicatorData, 
+                               InitialSpeciesData=InitialSpeciesData, ChooseFMult=ChooseFMult)
+     
+
+
+
+########## The following doesnt appear to be used by the model but perhaps I should double check where this information is coming from to ensure that these aren't set as defaults without my knowledge ??????
+
+
   # Guilds / functional groups from datfile (in this case each guild is a single species)
 Guildmembership <- dat$Guildmembership
 NGuild = length(unique(Guildmembership))
 # Initial values of depletion biomass for each guild
 Initvals <- dat$Initvals
-# Carrying capacity for each guild
-KGuild <- dat$KGuild
+
 # Total carrying capacity is sum of guild carrying capacity
 Ktot <- sum(KGuild)
 # Harvest rate for each species
@@ -50,46 +77,15 @@ r <- dat$r
 # Interactions as matrix
 BetweenGuildComp <- dat$BetweenGuildComp
 WithinGuildComp <- dat$WithinGuildComp
-# alpha is predation
-alpha <- dat$alpha
-spatial.overlap <- dat$spatial.overlap
-# Redefine parameter alpha and WithinGuildComp as products of matrices listed above
-alpha <- alpha*spatial.overlap
+
 WithinGuildComp <- WithinGuildComp*spatial.overlap
 # Redefine harvest rate as list of zeros
 hrate <- rep(0,Nsp)
-
-# Define BMSY and pick the species to include in the model
-SpeciesNames <- as.character(BMSYData[c(4,5,21,22,14,23,24,6,3,7),1]) # pick species names
-MeanTrophicLevel <- BMSYData[c(4,5,21,22,14,23,24,6,3,7),3] # ID mean trophic level for chosen species
-BMSY <- KGuild/2 # Set values for BMSY
-
-# Identify columns containing predator species
-Predators <- which(colSums(alpha)>0)
-# ???? I need the line above to be replaced with the line below
-# ??? Predators <- c("Predator1", "Predator2", "Predator3"...)
 
 #initial biomass for each species
 Nabund <- Initvals
 
 
-
-############## get historical time series of biomass and catch, 33 year of data####################
-# Define NI(abundandce index) for 33 years of data
-NI <- dat$NI
-# Remove the first column which represents year not initial abundance for a species
-NI <- NI[,-1]
-
-# Define CI(catch index) for 33 years of data
-CI <- dat$CI
-
-# Redefine functional groups
-theguilds <- c(1,1,2,2,1,3,3,1,1,1)  ##### ???????????????? this is very error prone, is there a way to get this information from BMSY above or one of the other documents, if yes then change the code for Pelagics below
-
-# Identify columns containing pelagic species
-Pelagics <- which(theguilds==2)
-# ???? I need the line above to be replaced with the line below
-# ??? Pelagics <- c("Pelagic1", "Pelagic2", "Pelagic3"...)
 
 
 
