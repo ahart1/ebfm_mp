106e0c977d4d2662a99b3638a7cdb662f0170884
Amanda R Hart <ahart2@umassd.edu>
2019-02-26 11:09:50 -0500

    I fixed several problems storing results and writing the .dat file required by the optimizer.

--- arhart/CatchCeilingPaper/ReferencePtCalc_2_22_19.R
+++ arhart/CatchCeilingPaper/ReferencePtCalc_2_22_19.R
@@ -42,6 +42,10 @@ EstimateRefPts <- function(WorkDir = NULL, datafile = NULL, Nsim = NULL){
   # Read in data
   dat <- fromJSON(datafile)
   
+  # Result storage
+  RefPts <- matrix(NA, nrow = Nsim, ncol = 5) # 5 = ncol(AggBiomass)
+  colnames(RefPts) <- c("PiscivoresBio", "BenthivoresBio", "PlanktivoresBio", "ElasmobranchsBio", "EcosystemBio")
+  
   for(i in 1:Nsim){
     print(i)
     
@@ -50,11 +54,11 @@ EstimateRefPts <- function(WorkDir = NULL, datafile = NULL, Nsim = NULL){
     colnames(Biomass) <- c("GB_Cod", "GB_Haddock", "Herring", "Mackerel", "Redfish", "Skates", "Spiny_dogfish","GB_WinterFlounder", "GB_YellowtailFlounder","GOM_GB_WindowpaneFlounder")
     Catch <- dat["TrueCatchResult"][[1]][[i]]  # This should give the first item (matrix of catch) for the ith simulation
     colnames(Catch) <- c("GB_Cod", "GB_Haddock", "Herring", "Mackerel", "Redfish", "Skates", "Spiny_dogfish","GB_WinterFlounder", "GB_YellowtailFlounder","GOM_GB_WindowpaneFlounder")
-    
-    # Pick last 10 years of data
-    EndBiomass <- Biomass[(nrow(Biomass)-9):nrow(Biomass),]
+
+    # Pick last 30 years of data
+    EndBiomass <- Biomass
     colnames(EndBiomass) <- c("GB_Cod", "GB_Haddock", "Herring", "Mackerel", "Redfish", "Skates", "Spiny_dogfish","GB_WinterFlounder", "GB_YellowtailFlounder","GOM_GB_WindowpaneFlounder")
-    EndCatch <- Catch[(nrow(Catch)-9):nrow(Catch),]
+    EndCatch <- Catch
     colnames(EndCatch) <- c("GB_Cod", "GB_Haddock", "Herring", "Mackerel", "Redfish", "Skates", "Spiny_dogfish","GB_WinterFlounder", "GB_YellowtailFlounder","GOM_GB_WindowpaneFlounder")
     
     # Calculate aggregate group and full ecosystem biomass
@@ -83,9 +87,6 @@ EstimateRefPts <- function(WorkDir = NULL, datafile = NULL, Nsim = NULL){
     AggCatch <- cbind(PiscivoresCat, BenthivoresCat, PlanktivoresCat, ElasmobranchsCat, EcosystemCat)
     colnames(AggCatch) <- c("PiscivoresCat", "BenthivoresCat", "PlanktivoresCat", "ElasmobranchsCat", "EcosystemCat") 
     
-    # Result storage
-    RefPts <- matrix(NA, nrow = Nsim, ncol = ncol(AggBiomass))
-    colnames(RefPts) <- colnames(AggBiomass)
     
     # Fit models to each of the aggregate/ecosystem getGroupMembers
     for(igroup in 1:ncol(AggBiomass)){
@@ -120,11 +121,11 @@ EstimateRefPts <- function(WorkDir = NULL, datafile = NULL, Nsim = NULL){
       write("# lyear",outfile,append=TRUE) # writes # lyear
       write(LastYear,outfile,append=TRUE) # places value for LastYear below #lyear
       write("# catches",outfile,append=TRUE) # writes # catches (here this is the OM catch)
-      write.table(round(AggCatch[,colnames(AggCatch)==igroup],digits=0),outfile,append=TRUE,row.names=FALSE,col.names=FALSE) # rounds the values of EndCatch to whole numbers in corresponding column
+      write.table(round(AggCatch[,igroup],digits=0),outfile,append=TRUE,row.names=FALSE,col.names=FALSE) # rounds the values of EndCatch to whole numbers in corresponding column
       write("# nbio",outfile,append=TRUE) # writes #nbio
       write(nrow(AggBiomass),outfile,append=TRUE) # places number of rows
       write("# obs bio",outfile,append=TRUE) # writes # obs bio (here this is the OM biomass)
-      write.table(cbind(1:nrow(AggBiomass), round(AggBiomass[,colnames(AggBiomass)==igroup],digits=0)), outfile, append=TRUE, row.names=FALSE, col.names=FALSE) # BioObs[,c(1,isp+1)
+      write.table(cbind(1:nrow(AggBiomass), round(AggBiomass[,igroup],digits=0)), outfile, append=TRUE, row.names=FALSE, col.names=FALSE) # BioObs[,c(1,isp+1)
       write("# obs cv",outfile,append=TRUE) # writes # obs cv
       write.table(cbind(1:nrow(AggBiomass),rep(0.25,nrow(AggBiomass))),outfile,append=TRUE,row.names=FALSE,col.names=FALSE)
       # ???? make the line above more general, why round years? this not necessary 
@@ -137,8 +138,8 @@ EstimateRefPts <- function(WorkDir = NULL, datafile = NULL, Nsim = NULL){
       # compile_admb(TPLFileName)
       run_admb(TPLFileName)
       
-      tempKpar <- read.table(paste0(TPLFileName,".par"),header=FALSE) # Need to label
-      tempRefPts <- exp(tempKpar[3,1]) # need to retransform so not in log space
+      tempKpar <- read.table(paste(TPLFileName,".par", sep=""),header=FALSE) # Need to label
+      tempRefPts <- exp(tempKpar$V1[3]) # need to retransform so not in log space
       RefPts[i, igroup] <- tempRefPts/4 # calculate reference point as half of Bmsy (K/2/2)
     }
   }
@@ -151,11 +152,12 @@ library(R2admb)
 library(jsonlite)
 setup_admb("/Applications/ADMBTerminal.app/admb") # This is the directory where the ADMB software files are stored
 compile_admb("single_schaef_copy")
+
 ##### Fit model to data and store ref pts #####
 AllRefPts <- NULL
 
 setwd("/Users/ahart2/Research/ebfm_mp/arhart/CatchCeilingPaper/UpdatedModel_Sim1000_Ceiling_AllInds_100PercentFmsy")
-#EstimateRefPts(datafile = "results50000.json", Nsim = 1)
+Results <- EstimateRefPts(datafile = "results200000.json", Nsim = 3)
 Results <- EstimateRefPts(datafile = "results50000.json", Nsim = 1000)
   AllRefPts <- rbind(AllRefPts, Results)
 Results <- EstimateRefPts(datafile = "results75000.json", Nsim = 1000)
